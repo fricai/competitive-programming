294dc6ed27ad4a29eb830c49f40ae23a0a147638
fricai <rishabh.dhiman.145@gmail.com>
2019-01-01 14:22:00 +0530

    Add solution for problem `A - PolandBall and Hypothesis` Link: https://codeforces.com/contest/755/problem/A Tags: brute force, graphs, math, number theory, *800 Ref: https://codeforces.com/contest/755/submission/47808038

--- codeforces/755/A.cpp
+++ codeforces/755/A.cpp
@@ -1,13 +1,9 @@
 #include <iostream>
 using namespace std;
 
-signed main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    
+int main() {
     int n; cin >> n;
-    
     if (n == 1) cout << 3;
-    else if (n == 2) cout << 4;
+    else if (n == 2) cout << 7;
     else cout << n - 2;
 }

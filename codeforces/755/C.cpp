d8fa5dabccee66329ecc67179b1703f089b18f9a
fricai <rishabh.dhiman.145@gmail.com>
2019-06-02 22:48:00 +0530

    Add solution for problem `C - PolandBall and Forest` Link: https://codeforces.com/contest/755/problem/C Tags: dfs and similar, dsu, graphs, interactive, trees, *1300 Ref: https://codeforces.com/contest/755/submission/54988777

--- codeforces/755/C.cpp
+++ codeforces/755/C.cpp
@@ -3,27 +3,20 @@
 #define REP(i, a, b) for (auto i = (a); i < (b); ++i)
 using namespace std;
 
-vector<int> succ, depth;
-
-void makeset(int n) {
-	succ.resize(n); REP(i, 0, n) succ[i] = i;
-	depth.resize(n, 1);
-}
+vector<int> succ, sizo;
 
 int find(int x) {
-	if (x != succ[x]) {
-		succ[x] = find(succ[x]);
-		return succ[x];
-	}
-	else return x;
+	int y = x;
+	while (y != succ[y]) y = succ[y];
+	succ[x] = y;
+	return y;
 }
 
-void link(int x, int y) {
+int link(int x, int y) {
 	x = find(x); y = find(y);
-
-	if (depth[x] > depth[y]) swap(x, y);
-	if (depth[x] == depth[y]) depth[y] = depth[x] + 1;
-	succ[x] = y;
+	if (sizo[y] > sizo[x]) swap(x, y); // sizo of x > sizo of y
+	succ[y] = x;
+	sizo[x] += sizo[y];
 }
 
 bool check(int x, int y) {
@@ -35,16 +28,14 @@ signed main() {
 	cin.tie(nullptr);
 
 	int n; cin >> n;
-	makeset(n);
-
+	succ.resize(n); sizo.resize(n, 1);
+	REP(i, 0, n) succ[i] = i;
 	REP(i, 0, n) {
 		int u; cin >> u; --u;
 		link(i, u);
 	}
-
 	vector<bool> ans(n, 0);
 	REP(i, 0, n) ans[find(i)] = 1;
-
 	int cnt = 0;
 	REP(i, 0, n) cnt += ans[i];
 	cout << cnt << '\n';

bffb11a772e95d2375af911fcd815bb4f1eca051
fricai <75023687+fricai@users.noreply.github.com>
2019-08-14 01:02:00 +0530

    Add solution for problem `C - Common Divisors` Link: https://codeforces.com/contest/1203/problem/C Tags: implementation, math, *1300 Ref: https://codeforces.com/contest/1203/submission/58781212

---
+++ codeforces/1203/C.cpp
@@ -0,0 +1,24 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#define REP(i, a, b) for (auto i = (a); i < (b); ++i)
+using namespace std;
+ 
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+	int n; cin >> n;
+	long long a[n];
+	REP(i, 0, n) cin >> a[i];
+	long long g = 0;
+	REP(i, 0, n) g = __gcd(g, a[i]);
+	long long ans = 0;
+	for (long long i = 1; i * i <= g; ++i) 
+		if (g % i == 0) { 
+			++ans;
+			if (g / i != i)
+				++ans;
+		}
+	cout << ans;
+}

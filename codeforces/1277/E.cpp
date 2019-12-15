b25f55e3df1210196588a4f31c9edc6728f12c2c
fricai <rishabh.dhiman.145@gmail.com>
2019-12-15 10:05:00 +0530

    Add solution for problem `E - Two Fairs` Link: https://codeforces.com/contest/1277/problem/E Tags: dfs and similar, graphs, *1900 Ref: https://codeforces.com/contest/1277/submission/66899479

--- codeforces/1277/E.cpp
+++ codeforces/1277/E.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 #include <vector>
-#include <queue>
 #define REP(i, a, b) for (auto i = (a); i < (b); ++i)
 using namespace std;
 
 const int N = 1 << 18;
+
 vector<int> g[N];
 bool vis[N];
+
+void dfs(int u) {
+	if (vis[u]) return;
+	vis[u] = true;
+	for (int v : g[u]) dfs(v);
+}
 
 signed main() {
 	ios_base::sync_with_stdio(false);
@@ -14,9 +20,7 @@ signed main() {
 
 	int t; cin >> t;
 	while (t--) {
-		int n, m, a[2];
-		
-		cin >> n >> m >> a[0] >> a[1]; --a[0], --a[1];
+		int n, m, a[2]; cin >> n >> m >> a[0] >> a[1]; --a[0], --a[1];
 		while (m--) {
 			int u, v; cin >> u >> v; --u, --v;
 			g[u].push_back(v);
@@ -24,21 +28,16 @@ signed main() {
 		}
 
 		int cnt[2];
-		auto bfs = [&](bool b) {
-			vis[a[!b]] = true; cnt[b] = 0;
-			queue<int> q; q.push(a[b]);
-			while (!q.empty()) {
-				int u = q.front(); q.pop();
-				if (vis[u]) continue;
-				vis[u] = true;
-				for (int v : g[u]) q.push(v);
-			}
-			REP(i, 0, n) {
+		auto stuff = [&](bool b) {
+			vis[a[!b]] = true;
+			dfs(a[b]);
+			cnt[b] = 0;
+			REP(i, 0, n)
 				if (!vis[i]) ++cnt[b];
 				else vis[i] = false;
-			}
 		};
-		bfs(0); bfs(1);
+		
+		stuff(0); stuff(1);
 		
 		cout << 1LL * cnt[0] * cnt[1] << '\n';
 		REP(i, 0, n) g[i].clear();

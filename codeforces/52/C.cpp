9c9ea12bf3f6be3da753f04ccf8203af8a4af637
fricai <rishabh.dhiman.145@gmail.com>
2020-01-08 18:07:00 +0530

    Add solution for problem `C - Circular RMQ` Link: https://codeforces.com/contest/52/problem/C Tags: data structures, *2200 Ref: https://codeforces.com/contest/52/submission/68399609

--- codeforces/52/C.cpp
+++ codeforces/52/C.cpp
@@ -4,84 +4,98 @@
 using namespace std;
 
 using ll = long long;
+
 const int N = 1 << 18;
-const int inf = 1e9;
-int n, a[N], u[N << 1]; ll t[N << 1];
+const ll inf = 1e16;
+int n, a[N];
 
-void build(int v = 1, int l = 0, int r = n - 1) {
-	u[v] = inf;
-	if (l == r) t[v] = a[l];
-	else {
-		int m = (l + r) >> 1;
-		build(v << 1, l, m);
-		build(v << 1|1, m + 1, r);
-		t[v] = min(t[v << 1], t[v << 1|1]);
+using pnode = struct node*;
+struct node {
+	static pnode nil;
+	ll v, u;
+	pnode l, r;
+	node(ll a = inf, ll b = inf) : v{a}, u{b}, l{nil}, r{nil} { }
+	node(pnode a, pnode b) : l{a}, r{b}, v{min(a->v, b->v)}, u{inf} {	}
+
+	void apply(int val) {
+		v += val;
+		if (u != inf) u += val;
+		else u = val;
 	}
-}
 
-void apply(int val, int v) {
-	t[v] += val;
-	if (u[v] != inf) u[v] += val;
-	else u[v] = val;
-}
-
-void push(int v) {
-	if (u[v] != inf) {
-		apply(u[v], v << 1);
-		apply(u[v], v << 1|1);
-		u[v] = inf;
+	void push() {
+		if (u != inf) {
+			l->apply(u);
+			r->apply(u);
+			u = inf;
+		}
 	}
-}
 
-void update(int val, int lo, int hi, int v = 1, int l = 0, int r = n - 1) {
-	if (l == lo && r == hi) {
-		apply(val, v);
-		return;
+	void update(int val, int lo, int hi, int st = 0, int en = n - 1) {
+		if (st == lo && en == hi) {
+			apply(val);
+			// cerr << st << ' ' << en << ' ' << v << ' ' << u << '\n';
+			return;
+		}
+		push();
+		int m = (st + en) / 2;
+		if (hi <= m) l->update(val, lo, hi, st, m);
+		else if (lo > m) r->update(val, lo, hi, m + 1, en);
+		else {
+			l->update(val, lo, m, st, m);
+			r->update(val, m + 1, hi, m + 1, en);
+		}
+		v = min(l->v, r->v);
+		u = inf;
 	}
-	push(v);
-	int m = (l + r) / 2;
-	if (hi <= m) update(val, lo, hi, v << 1, l, m);
-	else if (lo > m) update(val, lo, hi, v << 1|1, m + 1, r);
-	else {
-		update(val, lo, m, v << 1, l, m);
-		update(val, m + 1, hi, v << 1|1, m + 1, r);
+
+	ll query(int lo, int hi, int st = 0, int en = n - 1) {
+		if (st == lo && en == hi) return v;
+		push();
+		int m = (st + en) / 2;
+		if (hi <= m) return l->query(lo, hi, st, m);
+		if (lo  > m) return r->query(lo, hi, m + 1, en);
+		return min(l->query(lo, m, st, m), r->query(m + 1, hi, m + 1, en));
 	}
-	t[v] = min(t[v << 1], t[v << 1|1]);
-}
 
-ll query(int lo, int hi, int v = 1, int l = 0, int r = n - 1) {
-	if (lo == l && hi == r) return t[v];
-	push(v);
-	int m = (l + r) / 2;
-	if (hi <= m) return query(lo, hi, v << 1, l, m);
-	if (lo  > m) return query(lo, hi, v << 1|1, m + 1, r);
-	return min(query(lo, m, v << 1, l, m), query(m + 1, hi, v << 1|1, m + 1, r));
-}
+	void inc(int val, int l, int r) {
+		if (l > r) {
+			update(val, l, n - 1);
+			update(val, 0, r);
+		}	else update(val, l, r);
+	}
 
-void inc(int val, int l, int r) {
-	if (l > r) {
-		update(val, l, n - 1);
-		update(val, 0, r);
-	} else update(val, l, r);
-}
+	ll rmq(int l, int r) {
+		if (l > r)
+			return min(query(l, n - 1), query(0, r));
+		else
+			return query(l, r);
+	}
 
-ll rmq(int l, int r) {
-	if (l > r) return min(query(l, n - 1), query(0, r));
-	else return query(l, r);
-}
+	static pnode build(int st = 0, int en = n - 1) {
+		if (st == en) return new node(a[st]);
+		int m = (st + en) / 2;
+		return new node(build(st, m), build(m + 1, en));
+	}
+};
+pnode node::nil;
+pnode root;
 
 signed main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
 
-	cin >> n; rep(i, 0, n) cin >> a[i];
-	build();
+	node::nil = new node(); node::nil->l = node::nil->r = node::nil;
+	
+	cin >> n;
+	rep(i, 0, n) cin >> a[i];
+	root = node::build();
 	int q; cin >> q; cin.get();
 	while (q--) {
 		string s; getline(cin, s);
 		stringstream iss(s);
 		int l, r, v; iss >> l >> r;
-		if (iss >> v) inc(v, l, r);
-		else cout << rmq(l, r) << '\n';
+		if (iss >> v) root->inc(v, l, r);
+		else cout << root->rmq(l, r) << '\n';
 	}
 }

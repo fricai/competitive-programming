3d1506a026776ebbf4acb0ee39ec186f8f8dac1d
fricai <rishabh.dhiman.145@gmail.com>
2020-01-10 19:37:00 +0530

    Add solution for problem `A - Mezo Playing Zoma` Link: https://codeforces.com/contest/1285/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1285/submission/68498855

---
+++ codeforces/1285/A.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+
+signed main() {
+	cin.sync_with_stdio(0); cin.tie(0);
+	cin.exceptions(cin.failbit);
+
+	int n; cin >> n;
+	string s; cin >> s;
+	cout << n + 1;
+}

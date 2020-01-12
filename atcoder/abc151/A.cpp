bcec36f5c948990f13308f82c457f16fa90aeb7b
fricai <75023687+fricai@users.noreply.github.com>
2020-01-12 17:30:00 +0530

    Add solution for problem `A - Next Alphabet` Link: https://atcoder.jp/contests/abc151/tasks/abc151_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc151/submissions/9440281

---
+++ atcoder/abc151/A.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define trav(e, x) for (auto &e : x)
+#define dbg(x) #x << ": " << x <<
+#define sz(x) ((int)x.size())
+
+signed main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	char c; cin >> c; cout << (char)(c + 1);
+}

291b68344d7b29c0da47367f3ac5d2c060f6c86a
fricai <rishabh.dhiman.145@gmail.com>
2020-03-20 04:17:00 +0530

    Add solution for problem `K - Stones` Link: https://atcoder.jp/contests/dp/tasks/dp_k Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/11012790

---
+++ atcoder/dp/K.cpp
@@ -0,0 +1,43 @@
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <vector>
+
+using namespace std;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b) - 1; i >= (a); --i)
+#define trav(x, v) for (auto &x : v)
+
+#define sz(x) int((x).size())
+#define eb(x...) emplace_back(x)
+
+using ll = int64_t;
+using ld = long double;
+
+template<class T> bool ckmin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+constexpr ll inf = 2e18;
+constexpr int M = 1e9 + 7;
+
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n, K; cin >> n >> K;
+	int a[n]; rep(i, 0, n) cin >> a[i];
+	bool dp[K + 1];
+	rep(k, 0, K + 1) {
+		dp[k] = false;
+		rep(i, 0, n) {
+			if (a[i] > k) break;
+			if (!dp[k - a[i]]) {
+				dp[k] = true;
+				break;
+			}
+		}
+	}
+	cout << (dp[K] ? "First" : "Second");
+}

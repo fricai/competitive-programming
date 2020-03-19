2ad932c466007dce7ce541a181575163452926d6
fricai <75023687+fricai@users.noreply.github.com>
2020-03-19 12:51:00 +0530

    Add solution for problem `C - Vacation` Link: https://atcoder.jp/contests/dp/tasks/dp_c Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/11000793

--- atcoder/dp/C.cpp
+++ atcoder/dp/C.cpp
@@ -19,11 +19,12 @@ signed main() {
 	ll c[n][3];
 	rep(i, 0, n) rep(a, 0, 3) cin >> c[i][a];
 
-	ll dp[n + 1][3] = {{0, 0, 0}};
-	rep(i, 0, n) {
-		dp[i + 1][0] = max(dp[i][1], dp[i][2]) + c[i][0];
-		dp[i + 1][1] = max(dp[i][2], dp[i][0]) + c[i][1];
-		dp[i + 1][2] = max(dp[i][0], dp[i][1]) + c[i][2];
+	ll dp[n][3];
+	dp[0][0] = c[0][0];	dp[0][1] = c[0][1];	dp[0][2] = c[0][2];
+	rep(i, 1, n) {
+		dp[i][0] = max(dp[i - 1][1], dp[i - 1][2]) + c[i][0];
+		dp[i][1] = max(dp[i - 1][2], dp[i - 1][0]) + c[i][1];
+		dp[i][2] = max(dp[i - 1][0], dp[i - 1][1]) + c[i][2];
 	}
-	cout << max({dp[n][0], dp[n][1], dp[n][2]});
+	cout << max({dp[n - 1][0], dp[n - 1][1], dp[n - 1][2]});
 }

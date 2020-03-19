f96ce48cf1787afe1f73ad1776a7c5b3b85ab567
fricai <rishabh.dhiman.145@gmail.com>
2020-03-19 16:49:00 +0530

    Add solution for problem `E - Knapsack 2` Link: https://atcoder.jp/contests/dp/tasks/dp_e Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/11004582

--- atcoder/dp/E.cpp
+++ atcoder/dp/E.cpp
@@ -11,15 +11,16 @@ template<class T> bool ckmin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
 template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
 
 constexpr ll inf = 2e18;
+constexpr ll V = 1 << 17;
 
 signed main() {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 
-	int n, w, V = 1; cin >> n >> w;
-	ll v[n], c[n];
-	rep(i, 0, n) cin >> c[i] >> v[i], V += v[i];
-	ll dp[V] = {0};  rep(x, 1, V) dp[x] = inf;
+	int n, w; cin >> n >> w;
+	ll v[n], c[n], dp[V];
+	rep(i, 0, n) cin >> c[i] >> v[i];
+	dp[0] = 0; rep(x, 1, V) dp[x] = inf;
 	rep(i, 0, n) per(x, v[i], V) ckmin(dp[x], dp[x - v[i]] + c[i]);
 	per(x, 0, V) if (dp[x] <= w) return cout << x, 0;
 }

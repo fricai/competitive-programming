fa2ba39ef86c619f5260f22ea41fb3bcdc329b88
fricai <75023687+fricai@users.noreply.github.com>
2020-03-20 20:55:00 +0530

    Add solution for problem `Z - Frog 3` Link: https://atcoder.jp/contests/dp/tasks/dp_z Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/11029522

---
+++ atcoder/dp/Z.cpp
@@ -0,0 +1,64 @@
+#include <algorithm>
+#include <cassert>
+#include <set>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b) - 1; i >= (a); --i)
+#define trav(x, v) for (auto &x : v)
+
+#define sz(x) int((x).size())
+#define eb(x...) emplace_back(x)
+#define ff first
+#define ss second
+
+using ll = int64_t;
+const ll inf = 2e18;
+
+template<class T> bool ckmin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+struct Line {
+	mutable ll k, m, p;
+	bool operator<(const Line& o) const { return k < o.k; }
+	bool operator<(ll x) const { return p < x; }
+};
+struct LineContainer : multiset<Line, less<>> {
+	ll div(ll a, ll b) { return a / b - ((a ^ b) < 0 && a % b); }
+	bool isect(iterator x, iterator y) {
+		if (y == end()) return x->p = inf, false;
+		if (x->k == y->k) x->p = x->m > y->m ? inf : -inf;
+		else x->p = div(y->m - x->m, x->k - y->k);
+		return y->p <= x->p;
+	}
+	void add(ll k, ll m) {
+		auto z = insert({k, -m, 0}), y = z++, x = y;
+		while (isect(y, z)) z = erase(z);
+		if (x != begin() && isect(--x, y)) isect(x, y = erase(y));
+		while ((y = x) != begin() && y->p <= (--x)->p)
+			isect(x, erase(y));
+	}
+	ll query(ll x) {
+		assert(!empty());
+		auto l = *lower_bound(x);
+		return l.k * x + l.m;
+	}
+};
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n; ll c; cin >> n >> c;
+	ll h[n]; rep(i, 0, n) cin >> h[i];
+	LineContainer L; L.add(2 * h[0], h[0] * h[0]);
+	ll t = 0;
+	rep(j, 1, n) {
+		t = c + h[j] * h[j] - L.query(h[j]);
+		L.add(2 * h[j], t + h[j] * h[j]);
+	}
+	cout << t;
+}

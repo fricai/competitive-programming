95b61546c74ab756e8d13d5bd878ae5dd218f4e5
fricai <75023687+fricai@users.noreply.github.com>
2020-05-20 02:05:00 +0530

    Add solution for problem `D - Coffee Varieties (hard version)` Link: https://codeforces.com/contest/1290/problem/D Tags: constructive algorithms, graphs, interactive, *3000 Ref: https://codeforces.com/contest/1290/submission/80725457

--- codeforces/1290/D.cpp
+++ codeforces/1290/D.cpp
@@ -8,16 +8,20 @@ signed main() {
 	ios::sync_with_stdio(false);
 
 	int n, k, b; cin >> n >> k; b = n / k;
+	rep(z, 0, n) marked[z] = true;
 	rep(s, 0, b) {
-		cout << 'R' << endl;
-		for (int j = 0, t = s; j != b; ++j, t = (t + j) % b)
-			rep(z, t * k, (t + 1) * k)
-				if (!marked[z]) {
+		cout << 'R' << endl; int d = 0;
+		rep(i, 0, b) {
+			int t = (s + b + d) % b * k;
+			rep(z, t, t + k)
+				if (marked[z]) {
 					cout << "? " << z + 1 << endl;
 					char c; cin >> c;
-					marked[z] = c != 'N';
+					marked[z] = c != 'Y';
 				}
+			if (0 <= d) ++d; d = -d;
+		}
 	}
-	int ans = n; rep(z, 0, n) ans -= marked[z];
+	int ans = 0; rep(z, 0, n) ans += marked[z];
 	cout << "! " << ans << endl;
 }

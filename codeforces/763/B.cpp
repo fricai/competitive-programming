b852a6e67bbfc67d9ae342e104ca37c3a0086499
fricai <75023687+fricai@users.noreply.github.com>
2020-05-24 15:12:00 +0530

    Add solution for problem `B - Timofey and rectangles` Link: https://codeforces.com/contest/763/problem/B Tags: constructive algorithms, geometry, *2100 Ref: https://codeforces.com/contest/763/submission/81162941

--- codeforces/763/B.cpp
+++ codeforces/763/B.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 using namespace std;
- 
+
 const int inf = 1e9;
- 
+
 signed main() {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
- 
+
 	int n; cin >> n;
 	cout << "YES\n";
 	while (n--) {
 		int x, y, _; cin >> x >> y >> _ >> _;
 		x += inf; y += inf;
-		cout << (x << 1 & 2 | y & 1) + 1 << '\n';
+		cout << ((x & 1) << 1 | y & 1) + 1 << '\n';
 	}
 }

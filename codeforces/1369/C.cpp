92582b5bf3d12ef32c15bf39108bd5d252e18291
fricai <75023687+fricai@users.noreply.github.com>
2020-06-23 23:55:00 +0530

    Add solution for problem `C - RationalLee` Link: https://codeforces.com/contest/1369/problem/C Tags: greedy, math, sortings, two pointers, *1400 Ref: https://codeforces.com/contest/1369/submission/84828799

---
+++ codeforces/1369/C.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define trav(e, x) for (auto &e : x)
+#define eb(x...) emplace_back(x)
+#define all(x) begin(x), end(x)
+
+template<class T> bool ckmin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+const int N = 1 << 18;
+int a[N], w[N];
+
+ll solve() {
+	int n, k; cin >> n >> k;
+
+	rep(i, 0, n) cin >> a[i];
+	rep(i, 0, k) cin >> w[i];
+	sort(a, a + n);
+	sort(w, w + k);
+	ll s = 0;
+	for (int i = k, j = 0, b = n, r = n; i; j += w[i])
+		s += a[--r] + (--w[--i] ? a[j] : a[--b]);
+	return s;
+}
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int t; cin >> t;
+	while (t--) cout << solve() << '\n';	
+}

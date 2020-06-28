466641cdab42349a9869095bbfa17bccdbc7f045
fricai <75023687+fricai@users.noreply.github.com>
2020-06-28 20:14:00 +0530

    Add solution for problem `C - Move Brackets` Link: https://codeforces.com/contest/1374/problem/C Tags: greedy, strings, *1000 Ref: https://codeforces.com/contest/1374/submission/85305167

---
+++ codeforces/1374/C.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define trav(e, x) for (auto &e : x)
+#define eb(x...) emplace_back(x)
+#define all(x) begin(x), end(x)
+#define sz(x) int((x).size())
+
+template<class T> bool ckmin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+int solve() {
+	int n; cin >> n;
+	string s; cin >> s;
+	int cnt = 0, ans = 0;
+	rep(i, 0, n) {
+		if (s[i] == '(') ++cnt;
+		else --cnt;
+		if (cnt < 0) ++ans, cnt = 0;
+	}
+	return ans;
+}
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int t; cin >> t;
+	while (t--) cout << solve() << '\n';	
+}

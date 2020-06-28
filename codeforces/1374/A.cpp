6140e35105c3113a307d1918c3df5de5e6455539
fricai <75023687+fricai@users.noreply.github.com>
2020-06-28 20:08:00 +0530

    Add solution for problem `A - Required Remainder` Link: https://codeforces.com/contest/1374/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1374/submission/85297426

---
+++ codeforces/1374/A.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define trav(e, x) for (auto &e : x)
+#define eb(x...) emplace_back(x)
+#define all(x) begin(x), end(x)
+#define sz(x) int((x).size())
+
+template<class T> bool ckmin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int t; cin >> t;
+	while (t--) {
+		ll x, y, n; cin >> x >> y >> n;
+		ll k = (n/x + 10) * x + y;
+		while (k > n) k -= x;
+		cout << k << '\n';
+	}
+}

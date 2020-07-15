39c3b152f5dff1bceb5c93f509e2fc3a824fa7fb
fricai <rishabh.dhiman.145@gmail.com>
2020-07-16 00:20:00 +0530

    Add solution for problem `E - Duff in the Army` Link: https://codeforces.com/contest/588/problem/E Tags: data structures, dfs and similar, trees, *2200 Ref: https://codeforces.com/contest/588/submission/86938949

--- codeforces/588/E.cpp
+++ codeforces/588/E.cpp
@@ -1,26 +1,80 @@
 #include <algorithm>
+#include <cassert>
 #include <iostream>
 #include <vector>
 using namespace std;
 
 #define rep(i, a, b) for (auto i = (a); i < (b); ++i)
 #define trav(x, v) for (auto &x : v)
-#define sz(x) int((x).size())
 #define all(x) begin(x), end(x)
+#define sz(x) int((x).size())
+#define eb(x...) emplace_back(x)
 
-const int B = 17, N = 1 << B, K = 10;
-int k = K;
-vector<int> operator+(const vector<int> &a, const vector<int> &b) {
-	vector<int> v(sz(a) + sz(b));
-	merge(all(a), all(b), begin(v));
-	if (k < sz(v)) v.resize(k); return v;
-}
+const int N = 1 << 17, K = 10;
 
-vector<int> g[N], z[N][B + 1]; int a[N][B + 1], d[N];
-void dfs(int u, int p) {
-	a[u][0] = p; d[u] = d[p] + 1;
-	trav(v, g[u]) if (v != p) dfs(v, u);
+int sz[N], ch[N], pos[N], p[N], in[N], timer = 0;
+vector<int> g[N] = {{0}}, z[N];
+
+int lo, hi, k;
+struct T {
+	int n; vector<vector<int>> t;
+	vector<int> nodes;
+
+	void init() {
+		n = sz(nodes);
+		int sz = 1; while (sz < n) sz <<= 1;
+		t.resize(2 * sz);
+		build(1, 0, n);
+	}
+
+	void build(int v, int l, int r) {
+		if (r - l == 1) return void(t[v] = z[nodes[l]]);
+		int m = l + r >> 1;
+		build(v << 1|0, l, m); build(v << 1|1, m, r);
+		t[v].resize(sz(t[v << 1|0]) + sz(t[v << 1|1]));
+		merge(all(t[v << 1|0]), all(t[v << 1|1]), begin(t[v]));
+		if (K < sz(t[v])) t[v].resize(K);
+	}
+
+	void query(vector<int> &z, int v, int l, int r) {
+		if (r <= lo || hi <= l) return;
+		if (lo <= l && r <= hi) {
+			if (sz(t[v]) < k) z = {all(t[v])};
+			else z = {begin(t[v]), begin(t[v]) + k};
+			return;
+		}
+		vector<int> zl, zr; int m = l + r >> 1;
+		query(zl, v << 1|0, l, m); query(zr, v << 1|1, m, r);
+		z.resize(sz(zl) + sz(zr));
+		merge(all(zl), all(zr), begin(z));
+		if (k < sz(z)) z.resize(k);
+	}
+
+	vector<int> query(int l, int r) {
+		if (l > r) swap(l, r); lo = l; hi = r + 1;
+		vector<int> z; query(z, 1, 0, n);
+		return z;
+	}
+};
+vector<T> t;
+
+void dfs(int u, int a) {
+	in[u] = ++timer; sz[u] = 1; p[u] = a;
+	g[u].erase(find(all(g[u]), a));
+	if (g[u].empty()) ch[u] = sz(t), t.eb();
+	else {
+		int mx = g[u].front();
+		trav(v, g[u]) {
+			if (v == a) continue;
+			dfs(v, u); sz[u] += sz[v];
+			if (sz[mx] < sz[v]) mx = v;
+		}
+		ch[u] = ch[mx], pos[u] = pos[mx] + 1;
+	}
+	t[ch[u]].nodes.eb(u);
 }
+int head(int u) { return t[ch[u]].nodes.back(); }
+int ancestor(int u, int v) { return in[u] <= in[v] && in[v] < in[u] + sz[u]; }
 
 signed main() {
 	ios::sync_with_stdio(false);
@@ -29,33 +83,28 @@ signed main() {
 	int n, m, q; cin >> n >> m >> q;
 	rep(i, 1, n) {
 		int u, v; cin >> u >> v; --u; --v;
-		g[u].push_back(v); g[v].push_back(u);
+		g[u].eb(v); g[v].eb(u);
 	}
-	rep(i, 0, m) {
-		int x; cin >> x; --x;
-		if (sz(z[x][0]) < K) z[x][0].push_back(i + 1);
-	}
-
-	a[n][0] = n; dfs(0, n);
-	rep(j, 0, B)
-		rep(u, 0, n + 1)
-			a[u][j + 1] = a[a[u][j]][j],
-			z[u][j + 1] = z[a[u][j]][j] + z[u][j];
-
+	dfs(0, 0);
+	rep(i, 0, m) { int x; cin >> x; if (sz(z[--x]) < K) z[x].eb(i + 1); }
+	trav(v, t) v.init();
 	while (q--) {
 		int u, v; cin >> u >> v >> k; --u; --v;
-		vector<int> x; if (d[u] < d[v]) swap(u, v);
+		vector<int> x, z;
+		while (ch[u] != ch[v]) {
+			if (!ancestor(head(u), v)) {
+				z = t[ch[u]].query(pos[head(u)], pos[u]);
+				u = p[head(u)];
+			} else {
+				z = t[ch[v]].query(pos[head(v)], pos[v]);
+				v = p[head(v)];
+			}
+			x.insert(end(x), all(z));
+		}
+		z = t[ch[u]].query(pos[u], pos[v]);
+		x.insert(end(x), all(z));
 
-		for (int j = B; 0 <= j; --j)
-			if (d[v] <= d[a[u][j]])
-				x = x + z[u][j], u = a[u][j];
-		if (u != v) {
-			for (int j = B; 0 <= j; --j)
-				if (a[v][j] != a[u][j])
-					x = x + z[u][j] + z[v][j],
-					u = a[u][j], v = a[v][j];
-			x = x + z[u][1] + z[v][0];
-		} else x = x + z[u][0];
-		cout << sz(x); trav(v, x) cout << ' ' << v; cout << '\n';
+		sort(all(x)); if (k < sz(x)) x.resize(k);
+		cout << sz(x) << ' '; trav(z, x) cout << z << ' '; cout << '\n';
 	}
 }

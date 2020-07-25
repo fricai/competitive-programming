cbdd0cff89d869a317ea1e43e4f0b3dda8c40912
fricai <rishabh.dhiman.145@gmail.com>
2020-07-25 17:38:00 +0530

    Add solution for problem `C - Marks` Link: https://atcoder.jp/contests/m-solutions2020/tasks/m_solutions2020_c Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/m-solutions2020/submissions/15420223

---
+++ atcoder/m-solutions2020/C.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define trav(e, x) for (auto &e : x)
+#define eb(x...) emplace_back(x)
+#define all(x) begin(x), end(x)
+#define sz(x) int((x).size())
+
+template<class T> bool ckmin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n, k; cin >> n >> k;
+	vector<int> a(n);
+	trav(x, a) cin >> x;
+	rep(i, 0, n - k) cout << (a[i] < a[i + k] ? "Yes" : "No") << '\n';
+}

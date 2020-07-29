c4dcd4a511bc6b8ed1c6a4c12beb4314a5598815
fricai <75023687+fricai@users.noreply.github.com>
2020-07-29 23:53:00 +0530

    Add solution for problem `E - Calendar Ambiguity` Link: https://codeforces.com/contest/1389/problem/E Tags: math, number theory, *2200 Ref: https://codeforces.com/contest/1389/submission/88377862

---
+++ codeforces/1389/E.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define trav(e, x) for (auto &e : x)
+#define eb(x...) emplace_back(x)
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+
+template<class T> bool ckmin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+template<class T> T gcd(T a, T b) { return b != T(0) ? gcd(b, a % b) : a; }
+
+ll solve() {
+	ll m, d, w; cin >> m >> d >> w;
+	w /= gcd(w, d - 1); m = min(m, d);
+	return (m / w) * (m / w - 1) / 2 * w  + (m % w) * (m / w);
+}
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int t; cin >> t;
+	while (t--) cout << solve() << '\n';
+}

a8fc7500e87c8811c0e2b0ae34bff5cb165a68ed
fricai <75023687+fricai@users.noreply.github.com>
2020-07-30 22:00:00 +0530

    Add solution for problem `B - Captain Flint and a Long Voyage` Link: https://codeforces.com/contest/1388/problem/B Tags: greedy, math, *1000 Ref: https://codeforces.com/contest/1388/submission/88521050

---
+++ codeforces/1388/B.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define trav(e, x) for (auto &e : x)
+#define eb(x...) emplace_back(x)
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+
+template<class T> bool ckmin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+template<class T> T gcd(T a, T b) { return b != T(0) ? gcd(b, a % b) : a; }
+
+string solve() {
+	int n; cin >> n;
+	int m = n;
+	string bk = "";
+	while (m > 0) bk += "8", m -= 4;
+	n -= sz(bk); assert(n >= 0);
+	string s = "";
+	rep(i, 0, n) s += "9";
+	return s + bk;
+}
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int t; cin >> t;
+	while (t--) cout << solve() << '\n';
+}

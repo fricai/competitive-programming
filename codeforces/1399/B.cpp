a77a65766d3fc72527e9f668e6ec5972cb406063
fricai <rishabh.dhiman.145@gmail.com>
2020-08-05 20:13:00 +0530

    Add solution for problem `B - Gifts Fixing` Link: https://codeforces.com/contest/1399/problem/B Tags: greedy, *800 Ref: https://codeforces.com/contest/1399/submission/88967267

---
+++ codeforces/1399/B.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define trav(e, x) for (auto &e : x)
+#define eb(x...) emplace_back(x)
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+
+template<class T> bool ckmin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+template<class T> T gcd(T a, T b) { return b != T(0) ? gcd(b, a % b) : a; }
+
+const int N = 1 << 7;
+int a[N], b[N];
+
+ll solve() {
+	int n; cin >> n;
+	rep(i, 0, n) cin >> a[i];
+	rep(i, 0, n) cin >> b[i];
+	int mna = *min_element(a, a + n);
+	int mnb = *min_element(b, b + n);
+
+	ll ans = 0;
+	rep(i, 0, n) {
+		int x = a[i] - mna, y = b[i] - mnb;
+		ans += max(x, y);
+	}
+	return ans;
+}
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int t; cin >> t;
+	while (t--) cout << solve() << '\n';
+}

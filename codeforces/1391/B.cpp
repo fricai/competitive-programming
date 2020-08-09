3a28524c5cef5d0c7ac6767a2f976a04a42a2027
fricai <rishabh.dhiman.145@gmail.com>
2020-08-09 20:24:00 +0530

    Add solution for problem `B - Fix You` Link: https://codeforces.com/contest/1391/problem/B Tags: brute force, greedy, implementation, *800 Ref: https://codeforces.com/contest/1391/submission/89425889

---
+++ codeforces/1391/B.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define eb(x...) emplace_back(x)
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+#define lb(x...) lower_bound(x)
+#define ub(x...) upper_bound(x)
+
+template<class T> bool ckmin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+template<class T> T gcd(T a, T b) { return b != T(0) ? gcd(b, a % b) : a; }
+
+const int N = 1 << 7;
+int n, m;
+string g[N];
+
+int solve() {
+	cin >> n >> m;
+	rep(i, 0, n) cin >> g[i];
+
+	int ans = 0;
+	rep(i, 0, n - 1) {
+		if (g[i][m - 1] != 'D') ++ans;
+		g[i][m - 1] = 'D';
+	}
+	rep(j, 0, m - 1) {
+		if (g[n - 1][j] != 'R') ++ans;
+		g[n - 1][j] = 'R';
+	}
+	return ans;
+}
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int t; cin >> t;
+	while (t--) cout << solve() << '\n';	
+}

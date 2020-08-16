c7c09b7327330fff3716235bb5c2d42a088b5314
fricai <rishabh.dhiman.145@gmail.com>
2020-08-16 20:23:00 +0530

    Add solution for problem `C - Omkar and Waterslide` Link: https://codeforces.com/contest/1392/problem/C Tags: greedy, implementation, *1200 Ref: https://codeforces.com/contest/1392/submission/90104107

---
+++ codeforces/1392/C.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define eb(x...) emplace_back(x)
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+#define lb(x...) lower_bound(x)
+#define ub(x...) upper_bound(x)
+
+template<class T> bool ckmin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+template<class T> T gcd(T a, T b) { return b != T(0) ? gcd(b, a % b) : a; }
+
+const int N = 1 << 20;
+int a[N];
+
+ll solve() {
+	int n; cin >> n;
+	rep(i, 0, n) cin >> a[i];
+	ll z = 0;
+	for (int i = 0, j = 0; i < n; i = j) {
+		++j;
+		while (j < n && a[j - 1] <= a[j]) ++j;
+		if (j != n) z += a[j - 1] - a[j];
+	}
+	return z;
+}
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int t; cin >> t;
+	while (t--) cout << solve() << '\n';
+}

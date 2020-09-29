f2fef77e51ed7d5fdc4d44d30b4946c230ecb8a8
fricai <75023687+fricai@users.noreply.github.com>
2020-09-29 22:00:00 +0530

    Add solution for problem `E - Battle Lemmings` Link: https://codeforces.com/contest/1420/problem/E Tags: dp, greedy, *2500 Ref: https://codeforces.com/contest/1420/submission/94230718

---
+++ codeforces/1420/E.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define eb(x...) emplace_back(x)
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+#define lb(x...) lower_bound(x)
+#define ub(x...) upper_bound(x)
+
+template<class T> bool ckmin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+template<class T> T gcd(T a, T b) { return b != T(0) ? gcd(b, a % b) : a; }
+
+const int N = 90, inf = 1e9;
+int a[N];
+int dp[N][N][N * N];
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n; cin >> n;
+	rep(i, 0, n) cin >> a[i];
+
+	vector<int> x;
+	for (int i = 0, j = 0; i <= n; i = ++j) {
+		while (j < n && !a[j]) ++j;
+		x.eb(j - i);
+	}
+	
+	int z = sz(x), t = n - z + 1;
+	rep(i, 1, z) x[i] += x[i - 1];
+	rep(i, 0, z + 1)
+		rep(j, 0, t + 1)
+			rep(s, 0, n * (n - 1) / 2 + 1) dp[i][j][s] = inf;
+	dp[0][0][0] = 0;
+
+	rep(i, 0, z)
+		rep(j, 0, t + 1)
+			rep(s, 0, n * (n - 1) / 2 + 1)
+					rep(k, j, t + 1)
+						ckmin(dp[i + 1][k][s + abs(k - x[i])], dp[i][j][s] + (k - j) * (k - j));
+	int mn = inf;
+	rep(s, 0, n * (n - 1) / 2 + 1) {
+		ckmin(mn, dp[z][t][s]);
+		cout << (t * t - mn) / 2 << ' ';
+	}
+}

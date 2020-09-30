e8c98d179e13c2395c770cd49e1713b83c8af50f
fricai <rishabh.dhiman.145@gmail.com>
2020-10-01 03:01:00 +0530

    Add solution for problem `B - Arrays Sum` Link: https://codeforces.com/contest/1408/problem/B Tags: constructive algorithms, greedy, math, *1400 Ref: https://codeforces.com/contest/1408/submission/94360932

---
+++ codeforces/1408/B.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define eb(x...) emplace_back(x)
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+#define lb(x...) lower_bound(x)
+#define ub(x...) upper_bound(x)
+
+template<class T> bool ckmin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+template<class T> T gcd(T a, T b) { return b != T(0) ? gcd(b, a % b) : a; }
+
+const int N = 1 << 7;
+int a[N], b[N][N];
+
+int solve() {
+	int n, k; cin >> n >> k;
+	rep(i, 0, n) cin >> a[i];
+	n = unique(a, a + n) - a;
+	if (n <= k) return 1;
+	if (k == 1) return -1;
+	return (n - 1 + k - 1 - 1) / (k - 1);
+}
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int t; cin >> t;
+	while (t--) cout << solve() << '\n';
+}

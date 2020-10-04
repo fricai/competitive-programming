d9007e450ad884f43e0bfea8e6e038333e654a90
fricai <rishabh.dhiman.145@gmail.com>
2020-10-04 21:38:00 +0530

    Add solution for problem `A - Fence` Link: https://codeforces.com/contest/1422/problem/A Tags: geometry, math, *800 Ref: https://codeforces.com/contest/1422/submission/94663341

---
+++ codeforces/1422/A.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define eb(x...) emplace_back(x)
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+#define lb(x...) lower_bound(x)
+#define ub(x...) upper_bound(x)
+
+template<class T> bool ckmin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+template<class T> T gcd(T a, T b) { return b != T(0) ? gcd(b, a % b) : a; }
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int t; cin >> t;
+	while (t--) {
+		ll a, b, c; cin >> a >> b >> c;
+		ll d = max({0ll, a - b - c, b - c - a, c - a - b}) + 1;
+		assert(d < a + b + c);
+		cout << d << '\n';
+	}	
+}

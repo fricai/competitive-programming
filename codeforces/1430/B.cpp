4161a8426cc09f7a118f4adcb1e7cddc1267bdbf
fricai <rishabh.dhiman.145@gmail.com>
2020-10-14 00:31:00 +0530

    Add solution for problem `B - Barrels` Link: https://codeforces.com/contest/1430/problem/B Tags: greedy, implementation, sortings, *800 Ref: https://codeforces.com/contest/1430/submission/95455873

---
+++ codeforces/1430/B.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define eb(x...) emplace_back(x)
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+#define lb(x...) lower_bound(x)
+#define ub(x...) upper_bound(x)
+
+template<class T> bool ckmin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+ll solve() {
+	int n, k; cin >> n >> k;
+	vector<int> a(n);
+	for (auto &x : a) cin >> x;
+	sort(rall(a));
+	return accumulate(begin(a), begin(a) + k + 1, 0ll);
+}
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int t; cin >> t;
+	while (t--) cout << solve() << '\n';
+}

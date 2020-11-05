e88efdc572ce826eb69666fe6e66aee40c27ca65
fricai <rishabh.dhiman.145@gmail.com>
2020-11-05 16:17:00 +0530

    Add solution for problem `E - Solo mid Oracle` Link: https://codeforces.com/contest/1435/problem/E Tags: binary search, greedy, math, number theory, ternary search, *2100 Ref: https://codeforces.com/contest/1435/submission/97694815

---
+++ codeforces/1435/E.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define eb(x...) emplace_back(x)
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+#define lb(x...) lower_bound(x)
+#define ub(x...) upper_bound(x)
+
+template<class T> bool ckmin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+ll solve() {
+	ll a, b, c, d; cin >> a >> b >> c >> d;
+	if (a > b * c) return -1;
+	ll k = a / (b * d);
+	assert(b * d * k <= a);
+	return a * (k + 1) - k * (k + 1) / 2 * b * d;
+}
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int t; cin >> t;
+	while (t--) cout << solve() << '\n';
+}

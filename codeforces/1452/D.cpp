ea4ad28fb6b7dadc06e316ae3ea955f2c1b5e5a9
fricai <75023687+fricai@users.noreply.github.com>
2020-11-19 21:02:00 +0530

    Add solution for problem `D - Radio Towers` Link: https://codeforces.com/contest/1452/problem/D Tags: combinatorics, dp, math, *1600 Ref: https://codeforces.com/contest/1452/submission/98926676

--- codeforces/1452/D.cpp
+++ codeforces/1452/D.cpp
@@ -17,17 +17,26 @@ template<class T> bool ckmin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
 template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
 
 const int M = (119 << 23) + 1, N = 1 << 18;
+ll g[N];
 
 signed main() {
 	cin.tie(nullptr)->sync_with_stdio(false);
 
 	int n; cin >> n;
+	g[0] = 1;
 
-	ll p = (M + 1) / 2, q = p, g[2] = {1, 0};
-	for (int i = 1; i < n; ++i) {
-		g[i & 1] += g[~i & 1];
-		g[i & 1] %= M;
+	ll odd = 0, even = 1, p = (M + 1) / 2, q = 1;
+	for (int i = 1; i <= n; ++i) {
+		if (i & 1) {
+			g[i] = even;
+			odd += g[i];
+			odd %= M;
+		} else {
+			g[i] = odd;
+			even += g[i];
+			even %= M;
+		}
 		q = p * q % M;
 	}
-	cout << g[~n & 1] * q % M;
+	cout << g[n] * q % M;
 }

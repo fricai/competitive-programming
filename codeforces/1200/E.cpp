48f615a04f9e9a8c1a95e8bf0901d8f5b30c420f
fricai <75023687+fricai@users.noreply.github.com>
2020-12-01 19:29:00 +0530

    Add solution for problem `E - Compress Words` Link: https://codeforces.com/contest/1200/problem/E Tags: brute force, hashing, implementation, string suffix structures, strings, *2000 Ref: https://codeforces.com/contest/1200/submission/100119404

---
+++ codeforces/1200/E.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define eb(x...) emplace_back(x)
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+#define lb(x...) lower_bound(x)
+#define ub(x...) upper_bound(x)
+
+template<class T> bool ckmin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+const int N = 1 << 21;
+int lps[N];
+
+int search(string s) {
+	int n = s.length();
+	for (int i = 1, len = 0; i < n; ) {
+		if (s[i] == s[len]) lps[i++] = ++len;
+		else {
+			if (len != 0) len = lps[len - 1];
+			else lps[i++] = 0;
+		}
+	}
+	int res = lps[n - 1];
+	return res > n / 2 ? n / 2 : res;
+}
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n; cin >> n;
+	string res;
+	rep(i, 0, n) {
+		string s; cin >> s;
+		int x = min(sz(s), sz(res));
+		res += s.substr(search(s.substr(0, x) + '$' + res.substr(sz(res) - x)));
+	}
+	cout << res;
+}

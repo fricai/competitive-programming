6c48a389686f49d83a1caed12dc63f4aa3a4da60
fricai <75023687+fricai@users.noreply.github.com>
2020-12-03 16:00:00 +0530

    Add solution for problem `E - Phoenix and Berries` Link: https://codeforces.com/contest/1348/problem/E Tags: brute force, dp, greedy, math, *2400 Ref: https://codeforces.com/contest/1348/submission/100260951

--- codeforces/1348/E.cpp
+++ codeforces/1348/E.cpp
@@ -19,28 +19,45 @@ template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
 const int N = 1 << 9;
 const ll inf = 1e18;
 bool dp[N][N];
-int a[N], b[N];
+int rem[N][N], n, k, a[N], b[N];
+
+int sub(int a, int b) {
+	int t = a - b;
+	return t < 0 ? t + k : t;
+}
+int add(int a, int b) {
+	int t = a + b;
+	return k <= t ? t - k : t;
+}
 
 signed main() {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 
-	int n, k; cin >> n >> k; ll tot = 0;
+	cin >> n >> k; ll tot = 0;
 	for (int i = 1; i <= n; ++i) {
 		cin >> a[i] >> b[i];
 		tot += a[i] + b[i];
 	}
 
 	dp[0][0] = 1;
-	for (int i = 1; i <= n; ++i)
-		rep(j, 0, k)
-			rep(l, 0, min(k, a[i] + 1))
-				if (a[i] % k == l || k <= (a[i] - l) % k + b[i])
-					dp[i][j] |= dp[i - 1][((j - l) % k + k) % k];
+	for (int i = 1; i <= n; ++i) {
+		rep(j, 0, k) {
+			rem[i][j] = k - 1;
+			rep(l, 0, min(k, a[i] + 1)) {
+				if (a[i] % k == l || k <= (a[i] - l) % k + b[i]) {
+					if (dp[i - 1][sub(j, l)]) {
+						dp[i][j] = true;
+						ckmin(rem[i][j], (rem[i - 1][sub(j, l)] + b[i] + a[i] - l) % k);
+					}
+				}
+			}
+		}
+	}
 
 	ll ans = 0;
 	rep(j, 0, k)
 		if (dp[n][j])
-			ckmax(ans, (tot - j) / k);
+			ckmax(ans, (tot - j - rem[n][j]) / k);
 	cout << ans;
 }

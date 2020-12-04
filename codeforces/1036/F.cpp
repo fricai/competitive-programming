cac583279113070853c18e96abf7eb618c593118
fricai <75023687+fricai@users.noreply.github.com>
2020-12-04 11:59:00 +0530

    Add solution for problem `F - Relatively Prime Powers` Link: https://codeforces.com/contest/1036/problem/F Tags: combinatorics, math, number theory, *2400 Ref: https://codeforces.com/contest/1036/submission/100323673

---
+++ codeforces/1036/F.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define eb(x...) emplace_back(x)
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+#define lb(x...) lower_bound(x)
+#define ub(x...) upper_bound(x)
+
+template<class T> bool ckmin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+const int T = 1 << 17, X = 1 << 20;
+ll ans[T], z[X];
+int mu[] = {0, 1, -1, -1, 0, -1, 1, -1, 0, 0, 1, -1, 0, -1, 1, 1, 0, -1, 0, -1, 0, 1, 1, -1, 0, 0, 1, 0, 0, -1, -1, -1, 0, 1, 1, 1, 0, -1, 1, 1, 0, -1, -1, -1, 0, 0, 1, -1, 0, 0, 0, 1, 0, -1, 0, 1, 0, 1, 1, -1, 0, -1, 1, 0, 0, 1, -1, -1, 0, 1, -1, -1, 0, -1, 1, 0, 0, 1, -1};
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	ll mx = 0;
+	vector<pair<ll, int>> v;
+
+	int t; cin >> t;
+	rep(i, 0, t) {
+		ll n; cin >> n; ckmax(mx, n);
+		v.emplace_back(n, i);
+		ans[i] += n - floor(sqrt(n));
+	}
+	sort(all(v));
+
+	rep(i, 0ll, X) z[i] = i * i * i;
+
+	for (int x = X, j = 3; x != 2; ++j) {
+		int k = 0;
+		for (auto &[n, i] : v) {
+			while (z[k] <= n) ++k;
+			ans[i] += (k - 2) * mu[j];
+		}
+		rep(k, 2, x) {
+			z[k] *= k;
+			if (z[k] > mx) {
+				x = k;
+				break;
+			}
+		}
+	}
+
+	rep(i, 0, t) cout << ans[i] << '\n';
+}

dca9b3f1acfb110d8989448d5362a323f5c078d7
fricai <75023687+fricai@users.noreply.github.com>
2020-12-11 20:39:00 +0530

    Add solution for problem `B - Find the Spruce` Link: https://codeforces.com/contest/1461/problem/B Tags: brute force, dp, implementation, *1400 Ref: https://codeforces.com/contest/1461/submission/100930420

--- codeforces/1461/B.cpp
+++ codeforces/1461/B.cpp
@@ -19,24 +19,46 @@ template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
 const int N = 1 << 9;
 char g[N][N];
 int d[N][N];
+int n, m;
+
+bool ok(int i, int j, int l) {
+	if (l == 1) return true;
+	if (j + 2 * l - 1 > m + 1)
+		return false;
+	rep(k, 0, l) if (d[i][j + k] < k + 1)
+		return false;
+	for (int k = l; k <= 2 * (l - 1); ++k)
+		if (d[i][j + k] < -k + 2 * l - 1)
+			return false;
+	return true;
+}
+
 
 int solve() {
-	int n, m; cin >> n >> m;
+	cin >> n >> m;
+
 	int res = 0;
-
-	rep(i, 0, n) cin >> g[i];
-
-	rep(j, 0, m) d[n][j] = 0;
-	per(i, 0, n) {
-		rep(j, 0, m) {
-			if (g[i][j] != '*') {
-				d[i][j] = 0;
-				continue;
-			}
-			d[i][j] = 1 + (j > 0 && j < m - 1 ? min({d[i + 1][j - 1], d[i + 1][j], d[i + 1][j + 1]}) : 0); 
-			res += d[i][j];
+	for (int i = 1; i <= n; ++i)
+		for (int j = 1; j <= m; ++j) {
+			cin >> g[i][j];
+			if (g[i][j] != '*') d[i][j] = 0;
+			else d[i][j] = d[i - 1][j] + 1;
 		}
-	}	
+
+	for (int i = 1; i <= n; ++i) {
+		for (int j = 1; j <= m; ++j) {
+			if (g[i][j] != '*') { continue; }
+			int l = 1, r = 2 * n;
+			while (r - l > 1) {
+				int m = (l + r) / 2;
+				(ok(i, j, m) ? l : r) = m;
+			}
+			res += l;
+			// ok(i, j, 2);
+			// cerr << ok(i, j, 2) << ' ';
+		}
+		// cerr << '\n';
+	}
 	return res;
 }
 

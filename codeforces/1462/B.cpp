e54fe05e07033eab0e5e6a640e06c0532cb4930c
fricai <75023687+fricai@users.noreply.github.com>
2020-12-15 20:18:00 +0530

    Add solution for problem `B - Last Year's Substring` Link: https://codeforces.com/contest/1462/problem/B Tags: dp, implementation, strings, *800 Ref: https://codeforces.com/contest/1462/submission/101271808

---
+++ codeforces/1462/B.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define eb(x...) emplace_back(x)
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+#define lb(x...) lower_bound(x)
+#define ub(x...) upper_bound(x)
+
+template<class T> bool ckmin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+const string t = "2020";
+
+bool solve() {
+	int n; cin >> n;
+	string s; cin >> s;
+	if (n < 4) return false;
+	for (int i = 0; i <= 4; ++i)
+		if (s.substr(0, i) == t.substr(0, i) && s.substr(n - 4 + i) == t.substr(i))
+			return true;
+	return false;
+}
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int t; cin >> t;
+	while (t--) cout << (solve() ? "YES" : "NO") << '\n';
+}

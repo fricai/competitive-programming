9ec54b1c774417c586c022210942b89d8db9e98b
fricai <75023687+fricai@users.noreply.github.com>
2020-12-15 20:46:00 +0530

    Add solution for problem `E1 - Close Tuples (easy version)` Link: https://codeforces.com/contest/1462/problem/E1 Tags: binary search, combinatorics, math, sortings, two pointers, *1500 Ref: https://codeforces.com/contest/1462/submission/101297346

---
+++ codeforces/1462/E1.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define eb(x...) emplace_back(x)
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+#define lb(x...) lower_bound(x)
+#define ub(x...) upper_bound(x)
+
+template<class T> bool ckmin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+const int M = 1e9 + 7;
+const int N = 1 << 18;
+int a[N], nxt[N];
+ll f[N], fi[N], inv[N];
+
+ll nCr(ll n, int k) {
+	assert(k == 2);
+	return n * (n - 1) / 2;
+	// if (n < 0 || k > n || k < 0) return 0;
+	// return f[n] * fi[k] % M * fi[n - k] % M;
+}
+
+ll solve() {
+	int n; cin >> n;
+	// int m, k; cin >> m >> k;
+	int m = 3, k = 2;
+	rep(i, 0, n) cin >> a[i];
+	sort(a, a + n);
+
+	ll ans = 0;
+	for (int i = 0, j = 0; i < n; ++i) {
+		while (j < n && a[j] - a[i] <= k) ++j;
+		ans += nCr(j - i - 1, m - 1);
+			// if (M <= ans) ans -= M;
+	}
+	return ans;
+}
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	inv[1] = f[0] = f[1] = fi[0] = fi[1] = 1;
+	rep(i, 2, N) {
+		inv[i] = M - M / i * inv[M % i] % M;
+		f[i] = i * f[i - 1] % M;
+		fi[i] = inv[i] * fi[i - 1] % M;
+	}
+
+	int t; cin >> t;
+	while (t--) cout << solve() << '\n';
+}

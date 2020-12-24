0b1ddc2a49ffc31a4d47c011e4f4c79d316245a9
fricai <75023687+fricai@users.noreply.github.com>
2020-12-24 23:29:00 +0530

    Add solution for problem `E - Chocolate Bar` Link: https://codeforces.com/contest/598/problem/E Tags: brute force, dp, *2000 Ref: https://codeforces.com/contest/598/submission/102256701

--- codeforces/598/E.cpp
+++ codeforces/598/E.cpp
@@ -16,55 +16,37 @@ using ld = long double;
 template<class T> bool ckmin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
 template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
 
-const int N = 30, K = 50;
-int dp[N + 1][N + 1][K + 1];
-// int f(int n, int m, int k) {
-// 	if (k < 0 || k > n * m) return 1e9;
-// 	if (k == 0 || k == n * m) return 0;
-// 	if (dp[n][m][k] != -1) return dp[n][m][k];
+const int N = 40, K = 70;
+int dp[N][N][K];
+int f(int n, int m, int k) {
+	if (k < 0 || k > n * m) return 1e9;
+	if (k == 0 || k == n * m) return 0;
+	if (dp[n][m][k] != -1) return dp[n][m][k];
 	
-// 	int a = 1e9;
-// 	for (int i = 1; i <= m - i; ++i)
-// 		for (int r = 0; r <= k; ++r)
-// 			ckmin(a, f(n, i, r) + f(n, m - i, k - r));
-// 	a += n * n;
-
-// 	int b = 1e9;
-// 	for (int i = 1; i <= n - i; ++i)
-// 		for (int r = 0; r <= k; ++r)
-// 			ckmin(b, f(i, m, r) + f(n - i, m, k - r));
-// 	b += m * m;
-// 	return dp[n][m][k] = min(a, b);
-// }
+	int a = 1e9;
+	for (int i = 1; i <= m - i; ++i)
+		for (int r = 0; r <= k; ++r)
+			ckmin(a, f(n, i, r) + f(n, m - i, k - r));
+	a += n * n;
+	
+	int b = 1e9;
+	for (int i = 1; i <= n - i; ++i)
+		for (int r = 0; r <= k; ++r)
+			ckmin(b, f(i, m, r) + f(n - i, m, k - r));
+	b += m * m;
+	return dp[n][m][k] = min(a, b);
+}
 
 int solve() {
 	int n, m, k; cin >> n >> m >> k;
-	return dp[n][m][k];
+	return f(n, m, k);
 }
 
 signed main() {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 
-	for (int n = 1; n <= N; ++n) {
-		for (int m = 1; m <= N; ++m) {
-			for (int k = 1; k <= K; ++k) {
-
-				if (k == n * m) continue;
-
-				dp[n][m][k] = 1e9;
-				if (k > n * m) continue;
-				
-				for (int r = 0; r <= k; ++r) {
-					for (int i = 1; i <= n - i; ++i)
-						ckmin(dp[n][m][k], dp[i][m][r] + dp[n - i][m][k - r] + m * m);
-					for (int j = 1; j <= m - j; ++j)
-						ckmin(dp[n][m][k], dp[n][j][r] + dp[n][m - j][k - r] + n * n);
-				}
-			}
-		}
-	}
-
+	rep(i, 0, N) rep(j, 0, N) rep(k, 0, K) dp[i][j][k] = -1;
 	int t; cin >> t;
 	while (t--) cout << solve() << '\n';
 }

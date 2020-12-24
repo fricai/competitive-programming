757c4b96ece2e3f31daea38c27f53bd4ee185de6
fricai <75023687+fricai@users.noreply.github.com>
2020-12-25 02:15:00 +0530

    Add solution for problem `F - Pathwalks` Link: https://codeforces.com/contest/960/problem/F Tags: data structures, dp, graphs, *2100 Ref: https://codeforces.com/contest/960/submission/102265065

--- codeforces/960/F.cpp
+++ codeforces/960/F.cpp
@@ -15,51 +15,46 @@ using ld = long double;
 
 template<class T> bool ckmin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
 template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
-#include <ext/pb_ds/assoc_container.hpp>
-using namespace __gnu_pbds;
 
-struct chash {
-    const int RANDOM = (long long)(make_unique<char>().get()) ^ chrono::high_resolution_clock::now().time_since_epoch().count();
-    static unsigned long long hash_f(unsigned long long x) {
-        x += 0x9e3779b97f4a7c15;
-        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
-        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
-        return x ^ (x >> 31);
-    }
-    static unsigned hash_combine(unsigned a, unsigned b) { return a * 31 + b; }
-    int operator()(int x) const { return hash_f(x) ^ RANDOM; }
-};
+const int N = 1 << 17, M = 1 << 17, W = 1 << 17, NC = 6e6;
 
-const int N = 1 << 17, M = 1 << 17, W = 1 << 17;
+int t[NC], L[NC], R[NC], tot;
 
-struct {
-	gp_hash_table<int, int, chash> t;
-	int query(int l, int r) {
-		int res = 0;
-		for (l += W, r += W; l < r; l >>= 1, r >>= 1) {
-			if (l & 1) ckmax(res, t[l++]);
-			if (r & 1) ckmax(res, t[--r]);
-		}
-		return res;
+int dp_end[N], dp_start[N];
+
+void update(int v, int p, int x, int l = 0, int r = W) {
+	if (r - l == 1) return void(ckmax(t[v], x));
+	int m = (l + r) / 2;
+	if (p < m) {
+		if (!L[v]) L[v] = tot++;
+		update(L[v], p, x, l, m);
+	} else {
+		if (!R[v]) R[v] = tot++;
+		update(R[v], p, x, m, r);
 	}
-	void update(int p, int v) {
-		for (t[p += W] = v; p > 0; p >>= 1) t[p >> 1] = max(t[p], t[p ^ 1]);
-	}
-} st[N];
+	t[v] = max(t[L[v]], t[R[v]]);	
+}
+
+int query(int v, int lo, int hi, int l = 0, int r = W) {
+	if (!v || hi <= l || r <= lo) return 0;
+	if (lo <= l && r <= hi) return t[v];
+	int m = (l + r) / 2;
+	return max(query(L[v], lo, hi, l, m), query(R[v], lo, hi, m, r));
+}
 
 signed main() {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 
 	int n, m; cin >> n >> m;
+	tot = n + 1;
 
-    int ans = 0;
+	int ans = 0;
 	rep(e, 0, m) {
-        int a, b, w; cin >> a >> b >> w;
-        int cur = 1 + st[a].query(0, w);
-        st[b].update(w, cur);
-        ckmax(ans, cur);
-    }
-
+		int a, b, w; cin >> a >> b >> w;
+		int cur = 1 + query(a, 0, w);
+		update(b, w, cur);
+		ckmax(ans, cur);
+	}
 	cout << ans << '\n';
 }

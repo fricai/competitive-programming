d2d77321a31fb5af1dac3d1bcd4ab9435bd16c0f
fricai <75023687+fricai@users.noreply.github.com>
2020-12-25 15:08:00 +0530

    Add solution for problem `E - Necklace Assembly` Link: https://codeforces.com/contest/1367/problem/E Tags: brute force, dfs and similar, dp, graphs, greedy, number theory, *1900 Ref: https://codeforces.com/contest/1367/submission/102292050

---
+++ codeforces/1367/E.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = int64_t;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define trav(e, x) for (auto &e : x)
+
+template<class T> bool ckmin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+const int N = 1 << 11, A = 26;
+int f[A];
+
+int solve() {
+	int n, k; cin >> n >> k;
+	fill(f, f + A, 0);
+	rep(i, 0, n) {
+		char c; cin >> c;
+		++f[c - 'a'];
+	}
+
+	int ans = 1;
+	for (int m = 1; m <= n; ++m) {
+		int g = gcd(k, m);
+
+		priority_queue<int> pq;
+		rep(a, 0, A) pq.push(f[a]);
+		if ([&]() {
+			rep(i, 0, g) {
+				int t = pq.top(); pq.pop();
+				t -= m / g;
+				if (t < 0) return false;
+				pq.push(t);
+			}
+			return true;
+		}()) ans = m;
+	}
+
+	return ans;
+}
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int t; cin >> t;
+	while (t--) cout << solve() << '\n';	
+}

a4de7756f5a177bf72ca0b537e942555add33d44
fricai <75023687+fricai@users.noreply.github.com>
2021-01-06 18:05:00 +0530

    Add solution for problem `B - Kamil and Making a Stream` Link: https://codeforces.com/contest/1229/problem/B Tags: binary search, data structures, dfs and similar, dp, graphs, math, number theory, trees, *2000 Ref: https://codeforces.com/contest/1229/submission/103540079

--- codeforces/1229/B.cpp
+++ codeforces/1229/B.cpp
@@ -18,19 +18,40 @@ template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 
 const int B = 17, N = 1 << B, M = 1e9 + 7;
-ll x[N];
+int p[B][N];
+ll x[N], y[B][N];
 vector<int> g[N];
-ll ans;
+ll ans[N], d[N];
 
-void dfs(int u, map<ll, int> m) {
-    ++m[x[u]];
-    for (auto [val, cnt] : m) (ans += val * cnt) %= M;
+void init(int u) {
 	for (int v : g[u]) {
-        g[v].erase(find(all(g[v]), u));
-        map<ll, int> nxt;
-        for (auto [val, cnt] : m) nxt[gcd(x[v], val)] += cnt;
-        dfs(v, nxt);
-    }
+		g[v].erase(find(all(g[v]), u));
+		p[0][v] = u;
+		d[v] = d[u] + 1;
+		init(v);
+	}
+}
+
+void dfs(int u) {
+	if (x[u] == 0) ans[u] = ans[p[0][u]];
+	else {
+		ans[u] = 0;
+		ll z = x[u];
+		int v = u, pre = d[u];
+
+		// cerr << u << ": ";
+		while (v != 0) {
+			// cerr << v << ' ';
+			assert(z != 0);
+			per(j, 0, B) if (y[j][v] % z == 0) v = p[j][v];
+			(ans[u] += z % M * (pre - d[v] + 1)) %= M;
+			// cerr << v << ' ' << z << ' ' << (pre - d[v] + 1) << ' '<< z << "; ";
+			v = p[0][v];
+			pre = d[v];
+			z = gcd(z, x[v]);
+		}
+	}
+	for (int v : g[u]) dfs(v);
 }
 
 signed main() {
@@ -44,6 +65,21 @@ signed main() {
 		g[u].push_back(v);
 		g[v].push_back(u);
 	}
-    dfs(1, { });
-	cout << ans;
+	init(1);
+
+	rep(j, 1, B) for (int u = 1; u <= n; ++u) p[j][u] = p[j - 1][p[j - 1][u]];
+	for (int u = 1; u <= n; ++u) y[0][u] = gcd(x[p[0][u]], x[u]);
+
+	rep(j, 1, B) for (int u = 1; u <= n; ++u) y[j][u] = gcd(y[j - 1][p[j - 1][u]], y[j - 1][u]);
+	dfs(1);
+	// for (int u = 1; u <= n; ++u) {
+	// 	rep(j, 0, B) cerr << y[j][u] << ' ';
+	// 	cerr << '\n';
+	// }
+
+	ll fin = 0;
+	for (int u = 1; u <= n; ++u) fin = (fin + ans[u]) % M;
+	// cerr << '\n';
+	cout << fin;
+	// << '\n';
 }

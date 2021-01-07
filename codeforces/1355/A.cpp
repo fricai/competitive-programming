564cff3370566962448055c79eef0a5d06f73e97
fricai <rishabh.dhiman.145@gmail.com>
2021-01-07 18:15:00 +0530

    Add solution for problem `A - Sequence with Digits` Link: https://codeforces.com/contest/1355/problem/A Tags: brute force, implementation, math, *1200 Ref: https://codeforces.com/contest/1355/submission/103632422

---
+++ codeforces/1355/A.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+#define lb(x...) lower_bound(x)
+#define ub(x...) upper_bound(x)
+
+template<class T> bool ckmin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+int del(ll x) {
+	int mn = 10, mx = 0;
+	while (x) {
+		ckmin(mn, int(x % 10));
+		ckmax(mx, int(x % 10));
+		x /= 10;
+	}
+	return mn * mx;
+}
+
+ll solve() {
+	ll a; cin >> a;
+	ll k; cin >> k;
+	while (--k) {
+		if (del(a) == 0) break;	
+		a += del(a);
+	}
+	return a;
+}
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int t; cin >> t;
+	while (t--) cout << solve() << '\n';
+}

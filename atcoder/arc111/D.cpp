3e76223b5b67924461b9223091a2d4e1c38a5940
fricai <75023687+fricai@users.noreply.github.com>
2021-01-09 23:38:00 +0530

    Add solution for problem `D - Orientation` Link: https://atcoder.jp/contests/arc111/tasks/arc111_d Tags: AtCoder, *600 Ref: https://atcoder.jp/contests/arc111/submissions/19298230

--- atcoder/arc111/D.cpp
+++ atcoder/arc111/D.cpp
@@ -1,9 +1,9 @@
 #include <bits/stdc++.h>
- 
+
 using namespace std;
 using ll = long long;
 using ld = long double;
- 
+
 #define rep(i, a, b) for (auto i = (a); i < (b); ++i)
 #define per(i, a, b) for (auto i = (b); i-- > (a); )
 #define all(x) begin(x), end(x)
@@ -11,18 +11,18 @@ using ld = long double;
 #define sz(x) int((x).size())
 #define lb(x...) lower_bound(x)
 #define ub(x...) upper_bound(x)
- 
+
 template<class T> bool ckmin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
 template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
- 
+
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
- 
+
 const int N = 1 << 7, M = N * N / 2;
 vector<int> g[N];
 short dir[M];
 int c[N], a[M], b[M];
 bool vis[N];
- 
+
 void dfs(int u) {
     vis[u] = 1;
     for (int e : g[u]) {
@@ -32,7 +32,7 @@ void dfs(int u) {
         if (!vis[v]) dfs(v);
     }
 }
- 
+
 signed main() {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
@@ -48,7 +48,7 @@ signed main() {
         if (c[a[e]] > c[b[e]]) dir[e] = +1;
         if (c[a[e]] < c[b[e]]) dir[e] = -1;
     }
- 
+
     rep(u, 0, n) if (!vis[u]) dfs(u);
     rep(e, 0, m) cout << (dir[e] > 0 ? "->" : "<-") << '\n';
 }

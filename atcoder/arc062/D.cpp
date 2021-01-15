6cd6b8c0e26a29e22ef5043825bc5e03b8b707b1
fricai <rishabh.dhiman.145@gmail.com>
2021-01-15 15:00:00 +0530

    Add solution for problem `D - AtCoDeer and Rock-Paper` Link: https://atcoder.jp/contests/arc062/tasks/arc062_b Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/arc062/submissions/19442536

---
+++ atcoder/arc062/D.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+#define lb(x...) lower_bound(x)
+#define ub(x...) upper_bound(x)
+
+template<class T> bool ckmin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	string s; cin >> s;
+	int cnt = 0;
+
+	int ans = 0;
+	for (auto x : s) {
+		if (x == 'g') {
+			if (cnt > 0) {
+				--cnt;
+				++ans;
+			} else ++cnt;
+		} else {
+			if (cnt > 0) --cnt;
+			else {
+				++cnt;
+				--ans;
+			}
+		}
+	}
+	cout << ans;
+}

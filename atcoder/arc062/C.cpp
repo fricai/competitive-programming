f578c34c05d626c64487b6b84ef3da7c52245424
fricai <rishabh.dhiman.145@gmail.com>
2021-01-16 14:18:00 +0530

    Add solution for problem `C - AtCoDeer and Election Report` Link: https://atcoder.jp/contests/arc062/tasks/arc062_a Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/arc062/submissions/19457999

---
+++ atcoder/arc062/C.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+#define lb(x...) lower_bound(x)
+#define ub(x...) upper_bound(x)
+
+template<class T> bool ckmin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n; cin >> n;
+
+	ll x = 1, y = 1;
+	rep(i, 0, n) {
+		ll a, b;
+		cin >> a >> b;
+		ll m = max((x + a - 1) / a, (y + b - 1) / b);
+		x = a * m;
+		y = b * m;
+	}
+	cout << x + y << '\n';
+}

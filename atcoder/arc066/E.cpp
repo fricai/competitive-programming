a00bfb8b3553cfa8a59bb55cf9e6ecb44d4558a7
fricai <75023687+fricai@users.noreply.github.com>
2021-01-21 13:02:00 +0530

    Add solution for problem `E - Addition and Subtraction Hard` Link: https://atcoder.jp/contests/arc066/tasks/arc066_c Tags: AtCoder, *900 Ref: https://atcoder.jp/contests/arc066/submissions/19547705

---
+++ atcoder/arc066/E.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+#define lb(x...) lower_bound(x)
+#define ub(x...) upper_bound(x)
+
+template<class T> bool ckmin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const int N = 1 << 17;
+ll mx[2][N], mn[2][N];
+int a[N];
+char op[N];
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n; cin >> n;
+	cin >> a[0];
+	rep(i, 1, n) cin >> op[i - 1] >> a[i];
+	
+	per(i, 0, n) {
+		if (op[i] == '-') {
+			mn[0][i] = a[i] + mn[1][i + 1];
+			mx[0][i] = a[i] + mx[1][i + 1];
+
+			mn[1][i] = -a[i] + min(-mx[1][i + 1], mn[1][i + 1]);
+			mx[1][i] = -a[i] + max(-mn[1][i + 1], mx[1][i + 1]);
+		} else {
+			mn[0][i] = a[i] + mn[0][i + 1];
+			mx[0][i] = a[i] + mx[0][i + 1];
+
+			mn[1][i] = -a[i] + mn[1][i + 1];
+			mx[1][i] = -a[i] + max(mx[0][i + 1], mx[1][i + 1]);
+		}
+	}
+
+	cout << mx[0][0];
+}

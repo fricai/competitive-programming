53170d7e545a7fbab3b5350f32b93883ccc53981
fricai <75023687+fricai@users.noreply.github.com>
2021-01-21 16:20:00 +0530

    Add solution for problem `E - Pencils and Boxes` Link: https://codeforces.com/contest/985/problem/E Tags: binary search, data structures, dp, greedy, two pointers, *2100 Ref: https://codeforces.com/contest/985/submission/104976486

--- codeforces/985/E.cpp
+++ codeforces/985/E.cpp
@@ -18,8 +18,19 @@ template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 
 const int N = 1 << 19;
-bool dp[N];
+bool t[N << 1], dp[N];
 int a[N];
+
+bool query(int l, int r) {
+	bool res = 0;
+	for (l += N, r += N; l < r; l >>= 1, r >>= 1) {
+		if (l & 1) res |= t[l++];
+		if (r & 1) res |= t[--r];
+	}
+	return res;
+}
+
+void update(int p, bool b) { for (t[p |= N] = b; p >>= 1; ) t[p] = t[p << 1] | t[p << 1|1]; }
 
 signed main() {
 	ios::sync_with_stdio(false);
@@ -28,11 +39,16 @@ signed main() {
 	int n, k, d; cin >> n >> k >> d;
 	rep(i, 0, n) cin >> a[i + 1];
 	sort(a + 1, a + n + 1);
+
+	// update(0, 1);
 	dp[0] = 1;
 	for (int i = 1, j = 0, cnt = 0; i <= n; ++i) {
 		while (j <= i - k && a[i] - a[j + 1] > d) cnt -= dp[j++];
 		if (i - k >= 0) cnt += dp[i - k];
 		dp[i] = !!cnt;
+		// cerr << cnt << '\n';
+		// update(i, query(j - 1, i - k + 1));
 	}
 	cout << (dp[n] ? "YES" : "NO");
+	// cout << (query(n, n + 1) ? "YES" : "NO");
 }

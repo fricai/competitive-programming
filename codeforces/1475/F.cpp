019c9a3026f4a3f7d63a2407f13650ac1c94970a
fricai <rishabh.dhiman.145@gmail.com>
2021-01-25 22:39:00 +0530

    Add solution for problem `F - Unusual Matrix` Link: https://codeforces.com/contest/1475/problem/F Tags: 2-sat, brute force, constructive algorithms, *1900 Ref: https://codeforces.com/contest/1475/submission/105401066

---
+++ codeforces/1475/F.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+#define lb(x...) lower_bound(x)
+#define ub(x...) upper_bound(x)
+
+template<class T> bool ckmin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const int N = 1 << 10;
+bool g[N][N];
+bool s[N], t[N];
+bool solve() {
+	int n; cin >> n;
+	
+	rep(i, 0, n) {
+		rep(j, 0, n) {
+			char c; cin >> c;
+			g[i][j] = (c == '1');
+		}
+	}
+
+	rep(i, 0, n) {
+		rep(j, 0, n) {
+			char c; cin >> c;
+			g[i][j] ^= (c == '1');
+		}
+	}
+
+	rep(j, 0, n) s[j] = g[0][j];
+	rep(i, 0, n) t[i] = s[i] ^ g[i][i];
+	rep(i, 0, n) rep(j, 0, n) if (g[i][j] ^ t[i] ^ s[j]) return 0;
+	return 1;
+}
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int t; cin >> t;
+	while (t--) cout << (solve() ? "YES" : "NO") << '\n';
+}

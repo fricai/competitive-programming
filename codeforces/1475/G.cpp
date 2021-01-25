8746d2452d84ad03e23e95e09739a709129686c7
fricai <75023687+fricai@users.noreply.github.com>
2021-01-25 22:17:00 +0530

    Add solution for problem `G - Strange Beauty` Link: https://codeforces.com/contest/1475/problem/G Tags: dp, math, number theory, sortings, *1900 Ref: https://codeforces.com/contest/1475/submission/105394027

--- codeforces/1475/G.cpp
+++ codeforces/1475/G.cpp
@@ -18,11 +18,14 @@ template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 
 const int N = 1 << 18;
+vector<int> d[N];
 int cnt[N], dp[N], a[N];
 
 signed main() {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
+
+	rep(i, 1, N) for (int j = 2 * i; j < N; j += i) d[j].push_back(i);
 
 	int t; cin >> t;
 	while (t--) {
@@ -31,14 +34,15 @@ signed main() {
 		rep(i, 0, n) cin >> a[i];
 
 		fill(cnt, cnt + N, 0);
-		
-		rep(i, 0, n) ++cnt[a[i]];
-		rep(x, 1, N) dp[x] = cnt[x];
+		fill(dp, dp + N, 0);
 
+		rep(i, 0, n) ++cnt[a[i]];
+		
 		int ans = 0;
 		for (int x = 1; x < N; ++x) {
-			for (int y = x + x; y < N; y += x)
-				ckmax(dp[y], dp[x] + cnt[y]);
+			dp[x] = cnt[x];
+			for (int y : d[x])
+				ckmax(dp[x], dp[y] + cnt[x]);
 			ckmax(ans, dp[x]);
 		}
 		

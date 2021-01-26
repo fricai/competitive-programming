a32ecde408c29cea94e6d267b6e978e1a914a8f3
fricai <75023687+fricai@users.noreply.github.com>
2021-01-26 15:17:00 +0530

    Add solution for problem `C - Median Sum` Link: https://atcoder.jp/contests/agc020/tasks/agc020_c Tags: AtCoder, *700 Ref: https://atcoder.jp/contests/agc020/submissions/19705816

---
+++ atcoder/agc020/C.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+#define lb(x...) lower_bound(x)
+#define ub(x...) upper_bound(x)
+
+template<class T> bool ckmin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const int N = 1 << 11, MX = N * N;
+int a[N];
+bitset<MX> dp;
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n; cin >> n;
+	rep(i, 0, n) cin >> a[i];
+
+	dp.set(0);
+	int s = 0;
+	rep(i, 0, n) {
+		s += a[i];
+		dp |= dp << a[i];
+	}
+	rep(i, (s + 1) / 2, MX) if (dp.test(i)) return cout << i, 0;
+}

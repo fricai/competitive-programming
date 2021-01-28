1df63f80644a1bf6878e4e4dd3c76f32ce4f4d2c
fricai <rishabh.dhiman.145@gmail.com>
2021-01-28 20:51:00 +0530

    Add solution for problem `D - Nezzar and Board` Link: https://codeforces.com/contest/1478/problem/D Tags: constructive algorithms, math, number theory, *1800 Ref: https://codeforces.com/contest/1478/submission/105722470

---
+++ codeforces/1478/D.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+#define lb(x...) lower_bound(x)
+#define ub(x...) upper_bound(x)
+
+template<class T> bool ckmin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const int N  = 1 << 18;
+ll x[N];
+
+bool solve() {
+	int n; ll k; cin >> n >> k;
+	rep(i, 0, n) cin >> x[i];
+	sort(x, x + n);
+	ll g = 0;
+	rep(i, 1, n) g = gcd(g, abs(x[i] - x[i - 1]));
+	rep(i, 0, n) if (((k - x[i]) % g) == 0) return true;
+	return false;
+}
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int t; cin >> t;
+	while (t--) cout << (solve() ? "YES" : "NO") << '\n';
+}

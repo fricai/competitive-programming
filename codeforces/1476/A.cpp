a59bc8caff3eedac01bc5decb4cf7406827bf7e0
fricai <rishabh.dhiman.145@gmail.com>
2021-01-29 20:13:00 +0530

    Add solution for problem `A - K-divisible Sum` Link: https://codeforces.com/contest/1476/problem/A Tags: binary search, constructive algorithms, greedy, math, *1000 Ref: https://codeforces.com/contest/1476/submission/105857198

---
+++ codeforces/1476/A.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+#define lb(x...) lower_bound(x)
+#define ub(x...) upper_bound(x)
+
+template<class T> bool ckmin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+int solve() {
+	int n, k; cin >> n >> k;
+	if (n % k == 0) return 1;
+	if (k < n) return 2;
+	return (k + n - 1) / n;
+}
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int t; cin >> t;
+	while (t--) cout << solve() << '\n';
+}

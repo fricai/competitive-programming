0c2c5f8cc98f13d826a2b8c505cabf6c10039038
fricai <75023687+fricai@users.noreply.github.com>
2021-01-30 17:32:00 +0530

    Add solution for problem `A - Very Very Primitive Game` Link: https://atcoder.jp/contests/abc190/tasks/abc190_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc190/submissions/19773314

---
+++ atcoder/abc190/A.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+#define lb(x...) lower_bound(x)
+#define ub(x...) upper_bound(x)
+
+template<class T> bool ckmin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	 int a, b, c; cin >> a >> b >> c;
+	 if (c == 0) {
+		--a;
+		cout << (a < b ? "Aoki" : "Takahashi");
+	 } else {
+		 --b;
+		cout << (a <= b ? "Aoki" : "Takahashi");
+	 }
+}

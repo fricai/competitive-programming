d04c1c56ed524c9a8be0de1e2473b74f3c5b7359
fricai <75023687+fricai@users.noreply.github.com>
2021-02-01 13:32:00 +0530

    Add solution for problem `F - Many Moves` Link: https://atcoder.jp/contests/arc073/tasks/arc073_d Tags: AtCoder, *900 Ref: https://atcoder.jp/contests/arc073/submissions/19858245

--- atcoder/arc073/F.cpp
+++ atcoder/arc073/F.cpp
@@ -21,19 +21,66 @@ const int N = 1 << 18;
 
 const ll inf = 1e18;
 struct S {
-	ll t[N << 1];
-	S() { fill_n(t, 2 * N, inf); }
-	void set(int p, ll v) {
-		for (t[p |= N] = v; p >>= 1; )
-			t[p] = min(t[p << 1], t[p << 1|1]);
+	ll t[N << 1], lz[N << 1];
+
+	S() {
+		fill_n(t, 2 * N, inf);
+		fill_n(lz, 2 * N, 0);
 	}
-	ll query(int l, int r) {
-		ll res = inf;
-		for (l |= N, r |= N; l < r; l >>= 1, r >>= 1) {
-			if (l & 1) ckmin(res, t[l++]);
-			if (r & 1) ckmin(res, t[--r]);
+
+	void push(int v) {
+		t[v] += lz[v];
+		if (t[v] > inf) t[v] = inf;
+		if (v < N) {
+			lz[v << 1|0] += lz[v];
+			lz[v << 1|1] += lz[v];
 		}
-		return res;
+		lz[v] = 0;
+
+	}
+
+	int lo, hi;
+	ll x;
+
+	void upd(int v, int l, int r) {
+		push(v);
+		if (hi <= l || r <= lo) return;
+		if (lo <= l && r <= hi) return lz[v] += x, push(v);
+		int m = (l + r) / 2;
+		upd(v << 1, l, m); upd(v << 1|1, m, r);
+		t[v] = min(t[v << 1], t[v << 1|1]);
+	}
+
+	void add(int l, int r, ll v) {
+		lo = l; hi = r; x = v;
+		upd(1, 0, N);
+	}
+
+	void upd_set(int v, int l, int r) {
+		push(v);
+		if (lo < l || r <= lo) return;
+		if (r - l == 1) return void(t[v] = x);
+		int m = (l + r) / 2;
+		upd_set(v << 1, l, m); upd_set(v << 1|1, m, r);
+		t[v] = min(t[v << 1], t[v << 1|1]);
+	}
+
+	void set(int l, ll v) {
+		lo = l; x = v;
+		upd_set(1, 0, N);
+	}
+
+	ll query(int v, int l, int r) {
+		push(v);
+		if (hi <= l || r <= lo) return inf;
+		if (lo <= l && r <= hi) return t[v];
+		int m = (l + r) / 2;
+		return min(query(v << 1, l, m), query(v << 1|1, m, r));
+	}
+
+	ll query(int l, int r) {
+		lo = l; hi = r;
+		return query(1, 0, N);
 	}
 };
 
@@ -50,21 +97,25 @@ signed main() {
 	plus.set(b, abs(a - x) + b);
 	minus.set(a, abs(b - x) - a);
 	minus.set(b, abs(a - x) - b);
-	ll shift = 0;
-	
+
 	rep(i, 1, q) {
 		int y; cin >> y; --y;
-		ll to_place = min(minus.query(0, y) + y, plus.query(y, n) - y) - abs(x - y);
-		shift += abs(x - y);
+
+		ll to_place = min(minus.query(0, y) + y, plus.query(y, n) - y);
+
+		plus.add(0, n, abs(x - y));
+		minus.add(0, n, abs(x - y));
+
 		ll dpix = plus.query(x, x + 1) - x;
 		if (dpix > to_place) {
 			plus.set(x, to_place + x);
 			minus.set(x, to_place - x);
 		}
+
 		x = y;
 	}
-
+	
 	ll ans = inf;
 	rep(i, 0, n) ckmin(ans, plus.query(i, i + 1) - i);
-	cout << ans + shift;
+	cout << ans;
 }

1fd01bd277b9f99c69695efd0dbf1ef63522d94c
fricai <75023687+fricai@users.noreply.github.com>
2021-02-06 20:00:00 +0530

    Add solution for problem `D - Circle Lattice Points` Link: https://atcoder.jp/contests/abc191/tasks/abc191_d Tags: AtCoder, *400 Ref: https://atcoder.jp/contests/abc191/submissions/20009699

--- atcoder/abc191/D.py
+++ atcoder/abc191/D.py
@@ -9,5 +9,7 @@ right = floor(a + r)
 ans = 0
 for x in range(left, right + 1):
     d = (r * r - (a - x) * (a - x)).sqrt()
-    ans += floor(b + d) - ceil(b - d) + 1
+    L = ceil(b - d)
+    R = floor(b + d)
+    ans += R - L + 1
 print(ans)

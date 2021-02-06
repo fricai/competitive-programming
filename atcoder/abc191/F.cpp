e1f0812309818c7357f30cbb7538b914692c703e
fricai <75023687+fricai@users.noreply.github.com>
2021-02-06 20:24:00 +0530

    Add solution for problem `F - GCD or MIN` Link: https://atcoder.jp/contests/abc191/tasks/abc191_f Tags: AtCoder, *600 Ref: https://atcoder.jp/contests/abc191/submissions/20011180

---
+++ atcoder/abc191/F.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+#define lb(x...) lower_bound(x)
+#define ub(x...) upper_bound(x)
+
+template<class T> bool ckmin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n; cin >> n;
+	vector<int> a(n);
+	for (auto &x : a) cin >> x;
+
+	map<int, int> m;
+	for (auto x : a) {
+		auto f = [&](int d) {
+			auto &y = m[d];
+			y = gcd(y, x);
+		};
+		for (int d = 1; d * d <= x; ++d)
+			if (x % d == 0) f(d), f(x / d);
+	}
+
+	int mn = *min_element(all(a)), ans = 0;
+
+	for (auto [x, y] : m)
+		ans += x <= mn && x == y;
+	
+	cout << ans;
+}

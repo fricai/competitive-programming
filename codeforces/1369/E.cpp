53a609f45a67680206c9acb01bda64e2d7c4159e
fricai <75023687+fricai@users.noreply.github.com>
2021-03-06 14:25:00 +0530

    Add solution for problem `E - DeadLee` Link: https://codeforces.com/contest/1369/problem/E Tags: data structures, dfs and similar, greedy, implementation, sortings, *2400 Ref: https://codeforces.com/contest/1369/submission/109208682

--- codeforces/1369/E.cpp
+++ codeforces/1369/E.cpp
@@ -45,7 +45,7 @@ signed main() {
 		add(u);
 	}
 
-	deque<int> p;
+	vector<int> p;
 	while (!q.empty()) {
 		auto [del, u] = q.top(); q.pop();
 		if (vis[u]) continue;
@@ -54,12 +54,12 @@ signed main() {
 		for (auto e : g[u]) {
 			if (dead[e]) continue;
 			--deg[a[e]]; --deg[b[e]];
-			p.push_front(e);
+			p.push_back(e);
 		}
 		for (auto e : g[u]) if (!dead[e]) add(a[e] ^ b[e] ^ u), dead[e] = 1;
 	}
-
 	if (sz(p) < m) return cout << "DEAD", 0;
+	reverse(all(p));
 	cout << "ALIVE" << '\n';
 	for (auto x : p) cout << x + 1 << ' ';
 }

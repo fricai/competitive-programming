6275341b609b64e99f584cb9f6c806596451c02a
fricai <rishabh.dhiman.145@gmail.com>
2021-03-06 13:57:00 +0530

    Add solution for problem `E2 - Asterism (Hard Version)` Link: https://codeforces.com/contest/1371/problem/E2 Tags: binary search, combinatorics, dp, math, number theory, sortings, *2300 Ref: https://codeforces.com/contest/1371/submission/109206794

---
+++ codeforces/1371/E2.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+#define lb(x...) lower_bound(x)
+#define ub(x...) upper_bound(x)
+
+template<class T> bool ckmin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const int N = 1 << 17;
+int f[N + N], g[N];
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n, p; cin >> n >> p;
+	vector<int> a(n);
+	for (auto &x : a) cin >> x;
+	sort(all(a));
+
+	int R = a.back(), L = max(1, R - n);
+	for (int x = L, k = 0, i = 0; x < R + n; ++x, ++i) {
+		while (k < n && a[k] <= x) ++k;
+		f[i] = k % p;
+	}
+
+	vector<int> op;
+	for (int x = L, y = x, i = 0, j = 0; x <= R; ++x, ++i) {
+		for (; y - x < n; ++y, ++j) ++g[(y + p - f[j]) % p];
+		if (!g[x % p]) op.push_back(x);
+		--g[(x + p - f[i]) % p];
+	}
+
+	cout << sz(op) << '\n';
+	for (auto x : op) cout << x << ' ';
+}

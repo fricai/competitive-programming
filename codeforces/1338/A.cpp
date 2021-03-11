03c0bb12e9aec849e5a9291fbc9a82f22d6e5c26
fricai <rishabh.dhiman.145@gmail.com>
2021-03-11 11:14:00 +0530

    Add solution for problem `A - Powered Addition` Link: https://codeforces.com/contest/1338/problem/A Tags: greedy, math, *1500 Ref: https://codeforces.com/contest/1338/submission/109674619

---
+++ codeforces/1338/A.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+#define lb(x...) lower_bound(x)
+#define ub(x...) upper_bound(x)
+
+template<class T> bool ckmin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+int solve() {
+	int n; cin >> n;
+	vector<int> a(n);
+	for (auto &x : a) cin >> x;
+	int mx = 0;
+	rep(i, 1, n) {
+		int k = 0;
+		for (int z = max(0, a[i - 1] - a[i]); z; z >>= 1) ++k;
+		ckmax(mx, k);
+		ckmax(a[i], a[i - 1]);
+	}
+	return mx;
+}
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int t; cin >> t;
+	while (t--) cout << solve() << '\n';	
+}

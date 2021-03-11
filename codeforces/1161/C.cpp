13086b11efd6c16e879efb47ed6b9ad0f76a7404
fricai <rishabh.dhiman.145@gmail.com>
2021-03-11 22:54:00 +0530

    Add solution for problem `C - Thanos Nim` Link: https://codeforces.com/contest/1161/problem/C Tags: games, *2000 Ref: https://codeforces.com/contest/1161/submission/109727388

---
+++ codeforces/1161/C.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+#define lb(x...) lower_bound(x)
+#define ub(x...) upper_bound(x)
+
+template<class T> bool ckmin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n; cin >> n;
+	vector<int> a(n);
+	for (auto &x : a) cin >> x;
+	int z = *min_element(all(a)), c = 0;
+	for (auto x : a) c += z == x;
+	cout << (c <= n / 2 ? "Alice" : "Bob");
+}

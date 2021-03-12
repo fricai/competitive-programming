c55e94a32728868dd343003a45df3a0c6cbbddc8
fricai <75023687+fricai@users.noreply.github.com>
2021-03-12 18:03:00 +0530

    Add solution for problem `B - Good Triple` Link: https://codeforces.com/contest/1168/problem/B Tags: brute force, two pointers, *1900 Ref: https://codeforces.com/contest/1168/submission/109782120

---
+++ codeforces/1168/B.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+#define lb(x...) lower_bound(x)
+#define ub(x...) upper_bound(x)
+
+template<class T> bool ckmin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const int N = 3e5 + 10;
+vector<int> p[2];
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	string s; cin >> s;
+	int n = sz(s);
+	ll ans = n; ans *= n + 1; ans /= 2;
+	rep(l, 0, n) {
+		rep(r, l, n) {
+			if ([&]() {
+				for (int k = 1; r - 2 * k >= l; ++k)
+					if (s[r] == s[r - k] && s[r - k] == s[r - 2 * k])
+						return 1;
+				return 0;
+			}()) break;
+			--ans;
+		}
+	}
+
+	cout << ans;
+}

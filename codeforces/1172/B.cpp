8cf57711de293718f6e9a802bec4465dc5b0df20
fricai <75023687+fricai@users.noreply.github.com>
2021-03-14 22:56:00 +0530

    Add solution for problem `B - Nauuo and Circle` Link: https://codeforces.com/contest/1172/problem/B Tags: combinatorics, dfs and similar, dp, trees, *1900 Ref: https://codeforces.com/contest/1172/submission/109989272

---
+++ codeforces/1172/B.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+#define lb(x...) lower_bound(x)
+#define ub(x...) upper_bound(x)
+
+template<class T> bool ckmin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const int N = 1 << 18, M = (119 << 23) + 1;
+int deg[N];
+ll f[N];
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n; cin >> n;
+	f[0] = 1;
+	for (int i = 1; i <= n; ++i)
+		f[i] = f[i - 1] * i % M;
+
+	rep(e, 1, n) {
+		int u, v; cin >> u >> v;
+		++deg[u]; ++deg[v];
+	}
+	ll res = n;
+	for (int u = 1; u <= n; ++u)
+		res = res * f[deg[u]] % M;
+	cout << res << '\n';
+}

09d15f7c2f930bbb30baa22cc6c6ca0c1b052245
fricai <rishabh.dhiman.145@gmail.com>
2021-03-15 18:02:00 +0530

    Add solution for problem `A - Leha and Function` Link: https://codeforces.com/contest/840/problem/A Tags: combinatorics, greedy, math, number theory, sortings, *1300 Ref: https://codeforces.com/contest/840/submission/110036797

---
+++ codeforces/840/A.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+#define lb(x...) lower_bound(x)
+#define ub(x...) upper_bound(x)
+
+template<class T> bool ckmin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const int N = 1 << 18;
+int a[N];
+pair<int, int> b[N];
+int c[N];
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n; cin >> n;
+	rep(i, 0, n) cin >> a[i];
+	rep(i, 0, n) cin >> b[i].first, b[i].second = i;
+	sort(a, a + n);
+	sort(b, b + n, greater<>());
+	rep(i, 0, n) c[b[i].second] = a[i];
+	rep(i, 0, n) cout << c[i] << ' ';
+	cout << '\n';
+}

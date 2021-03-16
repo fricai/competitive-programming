db728b7ec4a0356bf6cc1d530cc44bc65296ce1e
fricai <rishabh.dhiman.145@gmail.com>
2021-03-16 18:17:00 +0530

    Add solution for problem `A - Convert to Ones` Link: https://codeforces.com/contest/997/problem/A Tags: brute force, greedy, implementation, math, *1500 Ref: https://codeforces.com/contest/997/submission/110112707

---
+++ codeforces/997/A.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+#define lb(x...) lower_bound(x)
+#define ub(x...) upper_bound(x)
+
+template<class T> bool ckmin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const int N = 3e5 + 10;
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n; ll x, y; cin >> n >> x >> y;
+	string s; cin >> s;
+	x = min(x, y);
+	int c = 0;
+	for (int i = 0, j = 0; i < n; i = j) {
+		while (j < n && s[i] == s[j]) ++j;
+		c += s[i] == '0';
+	}
+	cout << (c ? (c - 1) * x + y : 0) << '\n';
+}

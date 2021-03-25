110eb57765741c8ce872d75f92a89faa0032de86
fricai <75023687+fricai@users.noreply.github.com>
2021-03-25 20:07:00 +0530

    Add solution for problem `A - Strange Table` Link: https://codeforces.com/contest/1506/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1506/submission/110985649

---
+++ codeforces/1506/A.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+#define lb(x...) lower_bound(x)
+#define ub(x...) upper_bound(x)
+
+template<class T> bool ckmin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int t; cin >> t;
+	while (t--) {
+		ll n, m, x; cin >> n >> m >> x;
+		--x;
+		ll i = x % n, j = x / n;
+		ll z = m * i + j;
+		cout << z + 1 << '\n';
+	}
+}

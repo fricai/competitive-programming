1edb90e05fe47af6b67c0f1bc577a2d607c0acd0
fricai <75023687+fricai@users.noreply.github.com>
2021-04-17 12:56:00 +0530

    Add solution for problem `D - Nowhere P` Link: https://atcoder.jp/contests/jsc2021/tasks/jsc2021_d Tags: AtCoder, *400 Ref: https://atcoder.jp/contests/jsc2021/submissions/21812434

---
+++ atcoder/jsc2021/D.cpp
@@ -0,0 +1,32 @@
+#include <atcoder/modint>
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+#define lb(x...) lower_bound(x)
+#define ub(x...) upper_bound(x)
+
+template<class T> bool ckmin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const int M = 1e9 + 7;
+using mint = atcoder::static_modint<M>;
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n, p; cin >> n >> p;
+	mint a = p - 1;
+	mint b = p - 2;
+	cout << (a * b.pow(n - 1)).val() << '\n';
+}

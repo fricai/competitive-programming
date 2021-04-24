4608fee7050b7474e953ad58d0e5dd58c2c18b67
fricai <75023687+fricai@users.noreply.github.com>
2021-04-25 03:57:00 +0530

    Add solution for problem `D - RGB Coloring 2` Link: https://atcoder.jp/contests/abc199/tasks/abc199_d Tags: AtCoder, *400 Ref: https://atcoder.jp/contests/abc199/submissions/22047514

---
+++ atcoder/abc199/D.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+#define lb(x...) lower_bound(x)
+#define ub(x...) upper_bound(x)
+
+template<class T> bool ckmin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const int N = 21;
+vector<int> g[N];
+bool vis[N];
+
+vector<int> cc;
+void dfs(int u) {
+	cc.push_back(u);
+	vis[u] = 1;
+	for (int v : g[u]) if (!vis[v]) dfs(v);
+}
+
+int nxt(int x, int b) {
+	x += b + 1;
+	if (x > 3) x -= 3;
+	return x;
+}
+
+short col[N];
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n, m; cin >> n >> m;
+	rep(e, 0, m) {
+		int u, v; cin >> u >> v;
+		--u; --v;
+		g[u].push_back(v);
+		g[v].push_back(u);
+	}
+
+	ll res = 1;
+	rep(u, 0, n) {
+		if (vis[u]) continue;
+		cc.clear(); dfs(u);
+		int mx = sz(cc) - 1;
+		mx = 1 << mx;
+
+		ll tmp = 0;
+		
+		rep(S, 0, mx) {
+			for (auto u : cc) col[u] = 0;
+			
+			col[u] = 1;
+			bool valid = 1;
+			int T = S;
+			for (auto u : cc)
+				for (auto v : g[u])
+					if (col[v]) valid &= col[u] != col[v];
+					else col[v] = nxt(col[u], T & 1), T >>= 1;
+			tmp += valid;
+		}
+
+		res *= 3 * tmp;
+	}
+	cout << res << '\n';
+}

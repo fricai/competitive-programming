da016f9dabefff26211dd1254892ac86fc482bb4
fricai <75023687+fricai@users.noreply.github.com>
2021-05-01 19:35:00 +0530

    Add solution for problem `D - Message from Aliens` Link: https://atcoder.jp/contests/zone2021/tasks/zone2021_d Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/zone2021/submissions/22237338

---
+++ atcoder/zone2021/D.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+#define lb(x...) lower_bound(x)
+#define ub(x...) upper_bound(x)
+
+template<class T> bool ckmin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	string s; cin >> s;
+
+	deque<char> q;
+	
+	bool rev = 0;
+	for (auto c : s) {
+		if (c == 'R') rev ^= 1;
+		else {
+			if (rev) q.push_front(c);
+			else q.push_back(c);
+		}
+	}
+	if (rev) reverse(all(q));
+
+	string res;
+	for (auto c : q)
+		if (res.empty() || res.back() != c) res += c;
+		else res.pop_back();
+	
+	cout << res << '\n';
+}

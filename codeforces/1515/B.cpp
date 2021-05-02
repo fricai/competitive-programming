13fd351f5e40fbb90847fa062781c5545228b691
fricai <rishabh.dhiman.145@gmail.com>
2021-05-02 20:16:00 +0530

    Add solution for problem `B - Phoenix and Puzzle` Link: https://codeforces.com/contest/1515/problem/B Tags: brute force, geometry, math, number theory, *1000 Ref: https://codeforces.com/contest/1515/submission/114874657

---
+++ codeforces/1515/B.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+#define lb(x...) lower_bound(x)
+#define ub(x...) upper_bound(x)
+
+template<class T> bool ckmin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+bool ok(int x) {
+	int y = sqrt(x);
+	while (y * y < x) ++y;
+	while (y * y > x) --y;
+	return y * y == x;
+}
+
+bool solve() {
+	int n; cin >> n;
+	while (n % 2 == 0) {
+		if (ok(n / 2)) return 1;
+		n /= 2;
+	}
+	return 0;
+}
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int t; cin >> t;
+	while (t--) cout << (solve() ? "YES" : "NO") << '\n';
+}

6b1224dca1f63a8b3d1f625f6b9b92b4d21e188b
fricai <75023687+fricai@users.noreply.github.com>
2021-05-08 17:33:00 +0530

    Add solution for problem `B - 200th ABC-200` Link: https://atcoder.jp/contests/abc200/tasks/abc200_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc200/submissions/22397656

---
+++ atcoder/abc200/B.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+#define lb(x...) lower_bound(x)
+#define ub(x...) upper_bound(x)
+
+template<class T> bool ckmin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	ll n; int k; cin >> n >> k;
+	while (k--) {
+		if (n % 200 == 0) n /= 200;
+		else n = 1000 * n + 200;
+	}
+	cout << n;
+}

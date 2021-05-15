e1a55bdddac3f4d4d97d62c9dcb3de3f46076f28
fricai <75023687+fricai@users.noreply.github.com>
2021-05-16 01:53:00 +0530

    Add solution for problem `D - Game in Momotetsu World` Link: https://atcoder.jp/contests/abc201/tasks/abc201_d Tags: AtCoder, *400 Ref: https://atcoder.jp/contests/abc201/submissions/22646132

---
+++ atcoder/abc201/D.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+#define lb(x...) lower_bound(x)
+#define ub(x...) upper_bound(x)
+
+template<class T> bool ckmin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const int N = 1 << 11, inf = 1e9;
+int dp[N][N], a[N][N];
+
+void verdict(int x) {
+	if (x == 0) cout << "Draw\n";
+	else cout << (x > 0 ? "Takahashi" : "Aoki") << '\n';
+	exit(0);
+}
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n, m; cin >> n >> m;
+
+	auto f = [&](int i, int j) {
+		return (i ^ j) & 1;
+	};
+
+	rep(i, 0, n) rep(j, 0, m) {
+		char c; cin >> c;
+		a[i][j] = c == '+' ? +1 : -1;
+		dp[i][j] = f(i, j) ? inf : -inf;		
+	}
+
+	dp[n - 1][m - 1] = 0;
+	per(i, 0, n) {
+		per(j, 0, m) {
+			if (f(i, j)) {
+				int nxt = dp[i][j] + a[i][j];
+				if (i > 0) ckmax(dp[i - 1][j], nxt);
+				if (j > 0) ckmax(dp[i][j - 1], nxt);
+			} else {
+				int nxt = dp[i][j] - a[i][j];
+				if (i > 0) ckmin(dp[i - 1][j], nxt);
+				if (j > 0) ckmin(dp[i][j - 1], nxt);
+			}
+		}
+	}
+
+	verdict(dp[0][0]);
+}

160851ae630581ca4561346fda8d39de266b66c9
fricai <rishabh.dhiman.145@gmail.com>
2021-05-15 17:37:00 +0530

    Add solution for problem `B - Do you know the second highest mountain?` Link: https://atcoder.jp/contests/abc201/tasks/abc201_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc201/submissions/22606144

---
+++ atcoder/abc201/B.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+#define lb(x...) lower_bound(x)
+#define ub(x...) upper_bound(x)
+
+template<class T> bool ckmin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n; cin >> n;
+	vector<string> s(n);
+	vector<int> t(n);
+	rep(i, 0, n) cin >> s[i] >> t[i];
+	vector<int> ord(n);
+	iota(all(ord), 0);
+	sort(all(ord), [&](int x, int y) {
+		return t[x] > t[y];
+	});
+	cout << s[ord[1]] << '\n';
+}

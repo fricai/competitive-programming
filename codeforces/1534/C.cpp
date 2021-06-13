a84c4ca2128824e38e49177a189c3c4c7f8c9931
fricai <75023687+fricai@users.noreply.github.com>
2021-06-13 21:36:00 +0530

    Add solution for problem `C - Little Alawn's Puzzle` Link: https://codeforces.com/contest/1534/problem/C Tags: combinatorics, dp, dsu, graphs, math, *1300 Ref: https://codeforces.com/contest/1534/submission/119352044

---
+++ codeforces/1534/C.cpp
@@ -0,0 +1,110 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+#define lb(x...) lower_bound(x)
+#define ub(x...) upper_bound(x)
+
+template<class T> bool uin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
+template<class T> bool uax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+struct dsu {
+  public:
+    dsu() : _n(0) {}
+    explicit dsu(int n) : _n(n), parent_or_size(n, -1) {}
+
+    int merge(int a, int b) {
+        assert(0 <= a && a < _n);
+        assert(0 <= b && b < _n);
+        int x = leader(a), y = leader(b);
+        if (x == y) return x;
+        if (-parent_or_size[x] < -parent_or_size[y]) std::swap(x, y);
+        parent_or_size[x] += parent_or_size[y];
+        parent_or_size[y] = x;
+        return x;
+    }
+
+    bool same(int a, int b) {
+        assert(0 <= a && a < _n);
+        assert(0 <= b && b < _n);
+        return leader(a) == leader(b);
+    }
+
+    int leader(int a) {
+        assert(0 <= a && a < _n);
+        if (parent_or_size[a] < 0) return a;
+        return parent_or_size[a] = leader(parent_or_size[a]);
+    }
+
+    int size(int a) {
+        assert(0 <= a && a < _n);
+        return -parent_or_size[leader(a)];
+    }
+
+    std::vector<std::vector<int>> groups() {
+        std::vector<int> leader_buf(_n), group_size(_n);
+        for (int i = 0; i < _n; i++) {
+            leader_buf[i] = leader(i);
+            group_size[leader_buf[i]]++;
+        }
+        std::vector<std::vector<int>> result(_n);
+        for (int i = 0; i < _n; i++) {
+            result[i].reserve(group_size[i]);
+        }
+        for (int i = 0; i < _n; i++) {
+            result[leader_buf[i]].push_back(i);
+        }
+        result.erase(
+            std::remove_if(result.begin(), result.end(),
+                           [&](const std::vector<int>& v) { return v.empty(); }),
+            result.end());
+        return result;
+    }
+
+  private:
+    int _n;
+    // root node: -1 * component size
+    // otherwise: parent
+    std::vector<int> parent_or_size;
+};
+
+
+const int N = 1 << 20, M = 1e9 + 7;
+int p[N];
+
+int solve() {
+	int n; cin >> n;
+	vector<int> a(n), b(n);
+	for (auto &x : a) cin >> x, --x;
+	for (auto &x : b) cin >> x, --x;
+
+	dsu d(n + n);
+	rep(i, 0, n) d.merge(i, i + n);
+	vector<int> loc(n);
+	rep(i, 0, n) loc[a[i]] = i;
+	rep(i, 0, n) d.merge(i, loc[b[i]]);
+	return p[sz(d.groups())];
+}
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	p[0] = 1;
+	rep(i, 1, N) {
+		p[i] = p[i - 1] + p[i - 1];
+		if (M <= p[i]) p[i] -= M;
+	}
+
+	int t; cin >> t;
+	while (t--) cout << solve() << '\n';
+}

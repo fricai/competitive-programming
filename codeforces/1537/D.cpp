6ae95b7912565a05b97ccf4166451f1b0350d1e0
fricai <75023687+fricai@users.noreply.github.com>
2021-06-18 20:44:00 +0530

    Add solution for problem `D - Deleting Divisors` Link: https://codeforces.com/contest/1537/problem/D Tags: games, math, number theory, *1700 Ref: https://codeforces.com/contest/1537/submission/119845215

---
+++ codeforces/1537/D.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+#define lb(x...) lower_bound(x)
+#define ub(x...) upper_bound(x)
+
+template<class T> bool uin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
+template<class T> bool uax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+bool solve() {
+	// 1 if Alice wins, 0 if Bob
+	int n; cin >> n;
+	if (n % 2 != 0) return 0;
+	if ((n & (n - 1)) != 0) return 1;
+	int x = 0;
+	while (n != 0) ++x, n /= 2;
+	return x & 1;
+}
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int t; cin >> t;
+	while (t--) cout << (solve() ? "Alice" : "Bob") << '\n';	
+}

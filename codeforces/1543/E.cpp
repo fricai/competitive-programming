77b3972c99bc00f5953fd22ea09170f8899028a3
fricai <rishabh.dhiman.145@gmail.com>
2021-07-07 23:55:00 +0530

    Add solution for problem `E - The Final Pursuit` Link: https://codeforces.com/contest/1543/problem/E Tags: bitmasks, constructive algorithms, divide and conquer, graphs, greedy, math, *2700 Ref: https://codeforces.com/contest/1543/submission/121653632

--- codeforces/1543/E.cpp
+++ codeforces/1543/E.cpp
@@ -17,51 +17,52 @@ template<class T> bool uax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
 
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 
+const int N = 1 << 16;
+
 void solve() {
 	int k; cin >> k;
 	
 	int n = 1 << k, m = k * n / 2;
 	
 	vector<vector<int>> g(n);
-	
 	rep(e, 0, m) {
 		int u, v; cin >> u >> v;
 		g[u].push_back(v);
 		g[v].push_back(u);
 	}
 	
-	vector<int> pi(n, -1);
-	vector<bool> vis(n, 0);
-	queue<int> q;
-
-	pi[0] = 0; vis[0] = 1;
+	vector<int> pi(n, -1), cur;
+	
+	pi[0] = 0;
 	rep(i, 0, k) {
 		int u = g[0][i];
-		q.push(u);
+		cur.push_back(u);
 		pi[u] = 1 << i;
-		vis[u] = 1;
 	}
 	
-	while (!q.empty()) {
-		int u = q.front(); q.pop();
-		vis[u] = 1;
-		int pre = -1;
-		for (int v : g[u]) {
-			if (pi[v] != -1) {
-				if (pre < 0) pre = pi[v];
-				else pi[u] = pre | (pre ^ pi[v]);
-			} else if (!vis[v])
-				q.push(v), vis[v] = 1;
+	rep(b, 1, k) {
+		vector<int> nxt;
+		
+		for (auto u : cur) {
+			for (auto v : g[u]) {
+				if (pi[v] != -1) continue;	
+				for (auto w : g[v]) {
+					if (w == u || pi[w] == -1) continue;
+					pi[v] = pi[u] | (pi[u] ^ pi[w]);
+					nxt.push_back(v);
+					break;
+				}
+			}
 		}
+		
+		cur = move(nxt);
 	}
-	
+
 	vector<int> p(n);
 	rep(i, 0, n) p[pi[i]] = i;
 	for (auto x : p) cout << x << ' ';
 	cout << '\n';
-
-	if (k & (k - 1))
-		cout << "-1\n";
+	if (k & (k - 1)) cout << "-1\n";
 	else {
 		vector<int> c(n);
 		rep(u, 0, n)

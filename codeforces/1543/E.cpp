a120587366068399633e7a0ae0777d75cd021ccd
fricai <rishabh.dhiman.145@gmail.com>
2021-07-09 17:05:00 +0530

    Add solution for problem `E - The Final Pursuit` Link: https://codeforces.com/contest/1543/problem/E Tags: bitmasks, constructive algorithms, divide and conquer, graphs, greedy, math, *2700 Ref: https://codeforces.com/contest/1543/submission/121811012

--- codeforces/1543/E.cpp
+++ codeforces/1543/E.cpp
@@ -17,94 +17,82 @@ template<class T> bool uax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
 
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 
-const int N = 1 << 16;
-vector<int> g[N];
-bool vis[N];
+const int lgK = 4;
+int c[lgK + 1][1 << (1 << lgK)];
 
 void solve() {
 	int k; cin >> k;
 	
-	int n = 1 << k, m = k * n / 2;
+	int n = 1 << k, m = k << (k - 1);
+	
+	vector<vector<int>> g(n);
+	for (auto &v : g) v.reserve(k);
 
-	rep(u, 0, n) g[u].clear(), vis[u] = 0;
+	// rep(u, 0, n) rep(i, 0, k) g[u].push_back(u ^ (1 << i));
+
 	rep(e, 0, m) {
 		int u, v; cin >> u >> v;
 		g[u].push_back(v);
 		g[v].push_back(u);
 	}
 	
-	vector<int> p(n), pi(n, -1);
-	pi[0] = 0;
-	
-	vector<int> cur;
-	vector<int> cnt(n, 0);
+	vector<int> pi(n, -1);
+	queue<int> q;
 
-	vis[0] = 1;
+	pi[0] = 0;
 	rep(i, 0, k) {
 		int u = g[0][i];
-		cur.push_back(u);
+		q.push(u);
 		pi[u] = 1 << i;
-		cnt[u] = 1;
-		vis[u] = 1;
 	}
 	
-	rep(b, 1, k) {
-		vector<int> nxt;
-		
-		for (auto u : cur) {
-			assert(cnt[u] == b);
-			
-			int x = pi[u];
-			assert(x != -1);
-
-			for (auto v : g[u]) {
-				if (vis[v]) continue;
-				
-				for (auto w : g[v]) {
-					if (w == u || !vis[w]) continue;
-					assert(cnt[w] <= b);
-					
-					int y = pi[w];
-					int z = x ^ y;
-
-					assert(y != -1);
-		
-					assert(__builtin_popcount(z) == 2);
-
-					pi[v] = x | z;								
-					cnt[v] = b + 1;					
-					vis[v] = 1;
-					nxt.push_back(v);
-					break;
-				}
-			}
+	while (!q.empty()) {
+		int u = q.front(); q.pop();
+		int pre = -1;
+		for (int v : g[u]) {
+			if (0 <= pi[v]) {
+				if (pre < 0) pre = pi[v];
+				else pi[u] = pre | (pre ^ pi[v]);
+			} else if (pi[v] == -1)
+				q.push(v), pi[v] = -2;
 		}
-		
-		cur = move(nxt);
 	}
-
-
+	
+	vector<int> p(n);
 	rep(i, 0, n) p[pi[i]] = i;
 	for (auto x : p) cout << x << ' ';
 	cout << '\n';
+
 	if (k & (k - 1))
 		cout << "-1\n";
 	else {
-		vector<int> c(n);
-		rep(u, 0, n)
-			rep(i, 0, k)
-				if (u >> i & 1)
-					c[p[u]] ^= i;
-		for (auto x : c)
-			cout << x << ' ';
+		int j = 31 - __builtin_clz(k);
+		rep(u, 0, n) cout << c[j][pi[u]] << ' ';
 		cout << '\n';
+		
+		// vector<int> c(n);
+		// rep(u, 0, n)
+		// 	rep(i, 0, k)
+		// 		if (u >> i & 1)
+		// 			c[p[u]] ^= i;
+		// for (auto x : c)
+		// 	cout << x << ' ';
+		// cout << '\n';
 	}
 }
 
 signed main() {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
-
+	
+	c[0][0] = 0; c[0][1] = 0;
+	for (int b = 1, r = 0; r < lgK; b <<= 1, ++r)
+		rep(u, 0, 1 << b)
+			rep(v, 0, 1 << b) {
+				c[r + 1][v << b | u] = c[r][v] ^ c[r][u];
+				if (__builtin_popcount(v) & 1) c[r + 1][v << b | u] |= b;
+			}
+			
 	int t; cin >> t;
 	while (t--) solve();
 }

ad9d978e3eed3fe92386bc46a6d59328aed353ba
fricai <75023687+fricai@users.noreply.github.com>
2021-07-09 14:09:00 +0530

    Add solution for problem `C - Need for Pink Slips` Link: https://codeforces.com/contest/1543/problem/C Tags: bitmasks, brute force, dfs and similar, implementation, math, probabilities, *1900 Ref: https://codeforces.com/contest/1543/submission/121797260

--- codeforces/1543/C.py
+++ codeforces/1543/C.py
@@ -1,26 +1,24 @@
 from decimal import Decimal
 from functools import lru_cache
 
-eps = Decimal(1e-6)
-prec = 6
+one = Decimal(1)
 
 @lru_cache
 def g(c, p, v):
-    if abs(p - 1) <= eps:
-        return Decimal(1)
-    res = Decimal(1)
+    if p == one:
+        return one
+    res = one
     if c <= v:
         res += c
     else:
         res += c * g(c - v, p + v, v)
-    res = round(res, prec)
     return res
 
 @lru_cache
 def f(c, m, p, v):
-    if abs(p - 1) <= eps:
-        return Decimal(1)
-    res = Decimal(1)
+    if p == 1:
+        return one
+    res = one
     
     if m <= v:
         res += m * g(c + m / 2, p + m / 2, v)
@@ -31,7 +29,6 @@ def f(c, m, p, v):
         res += c * g(m + c / 2, p + c / 2, v)
     else:
         res += c * f(c - v, m + v / 2, p + v / 2, v)
-    res = round(res, prec)
     return res
 
 def solve():

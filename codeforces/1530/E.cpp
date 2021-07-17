1aab2f6bcbbf2be0c6806a1ad3555837fa26ddf9
fricai <rishabh.dhiman.145@gmail.com>
2021-07-17 22:37:00 +0530

    Add solution for problem `E - Minimax` Link: https://codeforces.com/contest/1530/problem/E Tags: constructive algorithms, greedy, strings, *2100 Ref: https://codeforces.com/contest/1530/submission/122849021

--- codeforces/1530/E.cpp
+++ codeforces/1530/E.cpp
@@ -19,13 +19,29 @@ mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 
 const int A = 26;
 
+int ans(string s) {
+    int n = (int)s.length();
+    vector<int> pi(n);
+    for (int i = 1; i < n; i++) {
+        int j = pi[i-1];
+        while (j > 0 && s[i] != s[j])
+            j = pi[j-1];
+        if (s[i] == s[j])
+            j++;
+        pi[i] = j;
+    }
+    return *max_element(all(pi));
+}
+
 string solve() {
 	string s; cin >> s;
 	int n = sz(s);
 	
-	if (n == 1)
+	if (n <= 2) {
+		sort(all(s));
 		return s;
-
+	}
+	
 	vector<int> f(A);
 	for (auto x : s) ++f[x - 'a'];
 
@@ -33,45 +49,70 @@ string solve() {
 		if (x == n)
 			return s;
 	
-	string t;
-	auto add = [&](int x) {
-		--f[x];
-		t.push_back(x + 'a');
-	};
+	int fst = -1;
+	per(i, 0, A)
+		if (f[i] == 1)
+			fst = i;
 	
-	rep(i, 0, A) {
-		if (f[i] != 1) continue;
-		add(i);
-		rep(j, 0, A)
-			while (f[j] > 0)
-				add(j);
+	if (fst >= 0) {
+		auto it = find(all(s), fst + 'a');
+		s.erase(it);
+		sort(all(s));
+		string t = string(1, fst + 'a') + s;
+
+		assert(ans(t) == 0);
+		
 		return t;
 	}
+
+	per(i, 0, A)
+		if (f[i])
+			fst = i;
 	
-	int a = 0;
-	while (f[a] == 0) ++a;
-	
-	if (f[a] - 2 <= (n - 2) / 2) {
-		add(a); add(a);
-		int i = a + 1;
-		while (f[a] > 0) {
-			while (f[i] == 0) ++i;
-			add(i); add(a);
+	if (f[fst] - 2 <= (n - 2) / 2) {
+		string t(2, 'a' + fst);
+		
+		f[fst] -= 2;
+		int i = 0;
+		while (f[fst] > 0) {
+			while (i < A && (i == fst || f[i] == 0)) ++i;
+			
+			assert(i < A);
+			
+			t.push_back(i + 'a');
+			t.push_back(fst + 'a');
+			
+			--f[i]; --f[fst];
 		}
+		
 		while (i < A) {
-			while (f[i] > 0)
-				add(i);
+			while (f[i] > 0) {
+				--f[i];
+				t.push_back(i + 'a');
+			}
 			++i;
 		}
+		
+		assert(sz(t) == n);
+		assert(ans(t) == 1);
 		return t;
 	}
-	
-	int b = a + 1;
+	int a = fst, b = fst + 1;
 	while (f[b] == 0) ++b;
-	add(a); add(b);
+	// start with ab
+	/*
+		Now ab must not occur again,
+	*/
 	
 	int c = b + 1;
 	while (c < A && f[c] == 0) ++c;
+	string t;
+	auto add = [&](int x) {
+		char c = x + 'a';
+		assert(f[x]);
+		--f[x]; t.push_back(c);
+	};
+	add(a); add(b);
 	
 	if (c < A) {
 		while (f[a] > 0) add(a);
@@ -83,7 +124,9 @@ string solve() {
 		while (f[b] > 0) add(b);
 		while (f[a] > 0) add(a);		
 	}
-	
+
+	assert(sz(t) == n);
+	assert(ans(t) == 1);
 	return t;
 }
 
@@ -92,5 +135,9 @@ signed main() {
 	cin.tie(nullptr);
 
 	int t; cin >> t;
-	while (t--) cout << solve() << '\n';
+	while (t--) {
+		auto s = solve();
+		// cerr << ans(s) << '\n';
+		cout << s << '\n';
+	}
 }

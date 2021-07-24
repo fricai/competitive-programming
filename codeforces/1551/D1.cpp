17fe7d44a6e0a2a4e824576ef722fe57c2385deb
fricai <75023687+fricai@users.noreply.github.com>
2021-07-24 20:40:00 +0530

    Add solution for problem `D1 - Domino (easy version)` Link: https://codeforces.com/contest/1551/problem/D1 Tags: constructive algorithms, math, *1700 Ref: https://codeforces.com/contest/1551/submission/123618424

---
+++ codeforces/1551/D1.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+#define lb(x...) lower_bound(x)
+#define ub(x...) upper_bound(x)
+
+template<class T> bool uin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
+template<class T> bool uax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+bool solve() {
+	int n, m, k; cin >> n >> m >> k;
+	
+	if (n & 1) {
+		swap(n, m);
+		k = (n * m) / 2 - k;
+	}
+	if (k & 1) return 0;
+	if ((m & 1) && (k > n / 2 * (m - 1)))
+		return 0;
+	return 1;
+}
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int t; cin >> t;
+	while (t--) cout << (solve() ? "YES" : "NO") << '\n';
+}

4257cfbfc8dc0722095754dbb901cedf751aa4e5
fricai <75023687+fricai@users.noreply.github.com>
2021-07-25 15:35:00 +0530

    Add solution for problem `F - Awesome Substrings` Link: https://codeforces.com/contest/1270/problem/F Tags: math, strings, *2600 Ref: https://codeforces.com/contest/1270/submission/123691624

---
+++ codeforces/1270/F.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+
+template<class T> bool uin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
+template<class T> bool uax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const int B = 375;
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	string s; cin >> s;
+	int n = sz(s);
+
+	vector<int> p(n + 1);
+	rep(i, 0, n) p[i + 1] = p[i] + (s[i] == '1');
+
+	// -n <= b * p[i] - i <= b * n
+	vector<int> cnt(n * B + n + 1, 0);
+
+	ll ans = 0;
+	for (int b = 1; b < min(B, n + 1); ++b) {
+		for (int i = 0; i <= n; ++i)
+			ans += cnt[b * p[i] - i + n]++;
+		for (int i = 0; i <= n; ++i)
+			cnt[b * p[i] - i + n]--;
+	}
+	
+	vector<int> L(n + 1), R(n + 1);
+	for (int i = 0; i <= n; ++i)
+		R[p[i]] = i;
+	for (int i = n; i >= 0; --i)
+		L[p[i]] = i;
+	
+	for (int i = 0; i <= n; ++i) {
+		for (int x = max(0, p[i] - n / B); x < p[i]; ++x) {
+			int del = p[i] - x;
+			ans += max(0, (i - L[x]) / del - max(B - 1, (i - R[x] - 1) / del));
+		}
+	}
+	
+	cout << ans << '\n';
+}

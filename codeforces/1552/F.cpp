f2bd061803f62058b713583e9e9e1e7d0d255459
fricai <rishabh.dhiman.145@gmail.com>
2021-07-26 00:30:00 +0530

    Add solution for problem `F - Telepanting` Link: https://codeforces.com/contest/1552/problem/F Tags: binary search, data structures, dp, sortings, *2200 Ref: https://codeforces.com/contest/1552/submission/123774053

--- codeforces/1552/F.cpp
+++ codeforces/1552/F.cpp
@@ -15,27 +15,41 @@ template<class T> bool uax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
 
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 
-const int M = 119 << 23 | 1;
+const int N = 1 << 18, inf = 1e9, M = 119 << 23 | 1;
 
 int add(int x, int y) { return x += y, x < M ? x : x - M; }
 int sub(int x, int y) { return x -= y, x < 0 ? x + M : x; }
 
+int t[N << 1];
+
+void update(int p, int v) {
+	for (t[p += N] = v; p >>= 1; )
+		t[p] = add(t[p << 1], t[p << 1|1]);
+}
+
+int query(int l, int r) {
+	int res = 0;
+	for (l += N, r += N; l < r; l >>= 1, r >>= 1) {
+		if (l & 1) res = add(res, t[l++]);
+		if (r & 1) res = add(res, t[--r]);
+	}
+	return res;
+}
+
 signed main() {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
-			
-	int n; cin >> n;
-	vector<int> p(n + 1), x(n);
-	int ans = 0;
-	for (int i = 0; i < n; ++i) {
-		int y, s; cin >> x[i] >> y >> s;
-		
-		int j = upper_bound(begin(x), begin(x) + i, y) - begin(x);
-		int cur = add(sub(x[i], y), sub(p[i], p[j]));
 
-		if (s) ans = add(ans, cur);
-		p[i + 1] = add(p[i], cur);
+	int n; cin >> n;
+	vector<int> x(n), y(n), s(n), p(n);
+	rep(i, 0, n) cin >> x[i] >> y[i] >> s[i];
+	rep(i, 0, n) p[i] = upper_bound(all(x), y[i]) - begin(x);
+	int ans = add(x[n - 1], 1);
+	for (int i = 0; i < n; ++i) {
+		int cur = add(sub(x[i], y[i]), query(p[i], i));
+		update(i, cur);
+		if (s[i]) ans = add(ans, cur);
 	}
-	ans = add(ans, add(x.back(), 1));
+
 	cout << ans << '\n';
 }

20c4110e6670bb35793b7e99541097d2f21ebe30
fricai <rishabh.dhiman.145@gmail.com>
2021-08-08 15:41:00 +0530

    Add solution for problem `B - A Determined Cleanup` Link: https://codeforces.com/contest/933/problem/B Tags: math, *2000 Ref: https://codeforces.com/contest/933/submission/125204879

---
+++ codeforces/933/B.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+
+template<class T> bool uin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
+template<class T> bool uax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	ll p; int k; cin >> p >> k;
+	vector<int> a;
+	while (p != 0) {
+		int x = p % k;
+		if (x < 0) x += k;
+		p = (x - p) / k;
+		a.push_back(x);
+	}
+	
+	cout << sz(a) << '\n';
+	for (auto x : a)
+		cout << x << ' ';
+	cout << '\n';
+}

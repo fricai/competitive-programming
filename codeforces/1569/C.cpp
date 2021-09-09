751ef2f3a8b9a585e2efe0ebf493e936e79cae84
fricai <75023687+fricai@users.noreply.github.com>
2021-09-09 22:37:00 +0530

    Add solution for problem `C - Jury Meeting` Link: https://codeforces.com/contest/1569/problem/C Tags: combinatorics, math, *1500 Ref: https://codeforces.com/contest/1569/submission/128366609

---
+++ codeforces/1569/C.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+
+template<class T> bool uin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
+template<class T> bool uax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+constexpr int M = 119 << 23 | 1;
+
+int mul(ll x, int y) { return x * y % M; }
+
+int solve() {
+	int n; cin >> n;
+	vector<int> a(n);
+	for (auto &x : a)
+		cin >> x;
+	nth_element(begin(a), begin(a), end(a), greater{});	
+	nth_element(begin(a), begin(a) + 1, end(a), greater{});
+	if (a[0] == a[1]) {
+		int f = 1;
+		for (int i = 1; i <= n; ++i)
+			f = mul(f, i);
+		return f;
+	}
+
+	if (a[0] - a[1] != 1)
+		return 0;
+
+	int cnt = count(all(a), a[1]);
+	int f = cnt;
+	for (int i = 1; i <= n; ++i)
+		if (i != cnt + 1)
+			f = mul(f, i);
+	return f;
+}
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int t; cin >> t;
+	while (t--) cout << solve() << '\n';
+}

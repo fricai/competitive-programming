04dfcd5903cc9c9baac750231ef506215e711cd7
fricai <rishabh.dhiman.145@gmail.com>
2021-09-14 17:36:00 +0530

    Add solution for problem `B - MIN-MEX Cut` Link: https://codeforces.com/contest/1566/problem/B Tags: bitmasks, constructive algorithms, dp, greedy, *800 Ref: https://codeforces.com/contest/1566/submission/128814777

---
+++ codeforces/1566/B.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+
+template<class T> bool uin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
+template<class T> bool uax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+int solve() {
+	string s; cin >> s;
+	int ans = 0;
+	for (int i = 0, j = 0; i < sz(s); i = j) {
+		while (j < sz(s) && s[i] == s[j]) ++j;
+		if (s[i] == '0') ++ans;
+	}
+	return min(2, ans);
+}
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int t; cin >> t;
+	while (t--) cout << solve() << '\n';
+}

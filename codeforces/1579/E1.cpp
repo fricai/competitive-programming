9774f88b8494480d5513f413924b299174c86991
fricai <75023687+fricai@users.noreply.github.com>
2021-09-28 20:29:00 +0530

    Add solution for problem `E1 - Permutation Minimization by Deque` Link: https://codeforces.com/contest/1579/problem/E1 Tags: constructive algorithms, greedy, math, *1000 Ref: https://codeforces.com/contest/1579/submission/130121567

---
+++ codeforces/1579/E1.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+
+template<class T> bool uin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
+template<class T> bool uax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+void solve() {
+	int n; cin >> n;
+	vector<int> p(n);
+	for (auto &x : p)
+		cin >> x;
+
+	deque<int> q;
+	for (auto x : p)
+		if (q.empty() || q.front() > x)
+			q.push_front(x);
+		else
+			q.push_back(x);
+
+	for (auto x : q)
+		cout << x << ' ';
+	cout << '\n';
+}
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int t; cin >> t;
+	while (t--) solve();
+}

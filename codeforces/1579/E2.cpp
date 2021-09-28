cdb6b72c6a0d9416e89ecb2946a1b54f6baa9682
fricai <rishabh.dhiman.145@gmail.com>
2021-09-28 22:34:00 +0530

    Add solution for problem `E2 - Array Optimization by Deque` Link: https://codeforces.com/contest/1579/problem/E2 Tags: data structures, greedy, *1700 Ref: https://codeforces.com/contest/1579/submission/130192662

---
+++ codeforces/1579/E2.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+
+template<class T> bool uin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
+template<class T> bool uax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+template<class T>
+using ost = __gnu_pbds::tree<T, __gnu_pbds::null_type, less<T>, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update>;
+
+ll solve() {
+	int n; cin >> n;
+	vector<int> p(n);
+	for (auto &x : p)
+		cin >> x;
+
+	ost<pair<int, int>> s;
+
+	ll ans = 0;
+	rep(i, 0, n) {
+		ans += min(s.order_of_key({p[i], -1}), i - s.order_of_key({p[i], n}));
+		s.insert({p[i], i});
+	}
+	return ans;
+}
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int t; cin >> t;
+	while (t--) cout << solve() << '\n';;
+}

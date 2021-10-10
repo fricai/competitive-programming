6973c20cbfd8ff277df8bbadeeab95e7b32ca77d
fricai <75023687+fricai@users.noreply.github.com>
2021-10-10 23:50:00 +0530

    Add solution for problem `A - Computer Game` Link: https://codeforces.com/contest/1598/problem/A Tags: dfs and similar, dp, implementation Ref: https://codeforces.com/contest/1598/submission/131491955

---
+++ codeforces/1598/A.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+
+template<class T> bool uin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
+template<class T> bool uax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+bool solve() {
+	int n; cin >> n;
+	string a, b; cin >> a >> b;
+	rep(i, 0, n)
+		if (a[i] == '1' && b[i] == '1')
+			return false;
+	return true;
+}
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int t; cin >> t;
+	while (t--) cout << (solve() ? "YES" : "NO") << '\n';
+}

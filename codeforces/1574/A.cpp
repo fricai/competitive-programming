03c53b081d95f29a6ac619fba0497dd1ae675e96
fricai <rishabh.dhiman.145@gmail.com>
2021-10-11 10:57:00 +0530

    Add solution for problem `A - Regular Bracket Sequences` Link: https://codeforces.com/contest/1574/problem/A Tags: constructive algorithms, *800 Ref: https://codeforces.com/contest/1574/submission/131516746

---
+++ codeforces/1574/A.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+
+template<class T> bool uin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
+template<class T> bool uax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+void solve() {
+	int n; cin >> n;
+	string s;
+	rep(i, 0, n) s += "()";
+
+	cout << s << '\n';
+	for (int i = 1; i + 1 < 2 * n; i += 2) {
+		swap(s[i], s[i + 1]);
+		cout << s << '\n';
+	}
+}
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int t; cin >> t;
+	while (t--) solve();
+}

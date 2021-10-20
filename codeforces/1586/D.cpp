babd01a8b560b5d046839044512144428c3f29a2
fricai <rishabh.dhiman.145@gmail.com>
2021-10-20 18:36:00 +0530

    Add solution for problem `D - Omkar and the Meaning of Life` Link: https://codeforces.com/contest/1586/problem/D Tags: constructive algorithms, greedy, interactive, math Ref: https://codeforces.com/contest/1586/submission/132530594

---
+++ codeforces/1586/D.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+
+template<class T> bool uin(T& a, const T& b) { return a > b ? a = b, true : false; }
+template<class T> bool uax(T& a, const T& b) { return a < b ? a = b, true : false; }
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+signed main() {
+	ios::sync_with_stdio(false);
+
+	int n; cin >> n;
+
+	vector<int> b(n + 1);
+
+	for (int i = 1; i <= n; ++i) {
+		cout << "?";
+		for (int j = 1; j <= n; ++j)
+			cout << ' ' << 1 + (i == j);
+		cout << endl;
+
+		int k;
+		cin >> k;
+		if (k != 0 && k != i)
+			b[i] = k;
+	}
+
+	for (int i = 1; i <= n; ++i) {
+		cout << "?";
+		for (int j = 1; j <= n; ++j)
+			cout << ' ' << 2 - (i == j);
+		cout << endl;
+
+		int k;
+		cin >> k;
+		if (k != 0 && k != i)
+			b[k] = i;
+	}
+
+	vector<int> p(n + 1);
+
+	vector<bool> in_b(n + 1, false);
+	for (int i = 1; i <= n; ++i)
+		in_b[b[i]] = true;
+	
+	int one = 1;
+	while (in_b[one]) ++one;
+
+	for (int x = 1; x <= n; one = b[one], ++x) {
+		p[one] = x;
+	}
+
+	cout << "!";
+	for (int i = 1; i <= n; ++i)
+		cout << ' ' << p[i];
+	cout << endl;
+}

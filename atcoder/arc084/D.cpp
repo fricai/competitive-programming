5880319bdc8c25a384166b1cfc88e653ffc4d8e8
fricai <75023687+fricai@users.noreply.github.com>
2021-11-26 22:57:00 +0530

    Add solution for problem `D - Small Multiple` Link: https://atcoder.jp/contests/arc084/tasks/arc084_b Tags: AtCoder, *700 Ref: https://atcoder.jp/contests/arc084/submissions/27502449

---
+++ atcoder/arc084/D.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+
+template<class T> bool uin(T& a, const T& b) { return a > b ? a = b, true : false; }
+template<class T> bool uax(T& a, const T& b) { return a < b ? a = b, true : false; }
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+constexpr int inf = 1e9;
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int k; cin >> k;
+	vector<vector<pair<int, bool>>> g(k);
+	rep(i, 0, k) g[i].emplace_back((i + 1) % k, 1);
+	rep(i, 0, k) g[i].emplace_back(10 * i % k, 0);
+
+	vector<int> d(k, inf);
+	deque<int> q;
+	q.emplace_back(1);
+	d[1] = 1;
+	while (!q.empty()) {
+		int u = q.front();
+		q.pop_front();
+		for (auto [v, cost] : g[u]) {
+			if (uin(d[v], d[u] + cost)) {
+				if (cost)
+					q.push_back(v);
+				else
+					q.push_front(v);
+			}
+		}
+	}
+	cout << d[0] << '\n';
+}

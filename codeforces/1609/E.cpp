7601035681f5690b63c39cf7f6a1e0b9d1ef5ef7
fricai <rishabh.dhiman.145@gmail.com>
2021-12-02 12:13:00 +0530

    Add solution for problem `E - William The Oblivious ` Link: https://codeforces.com/contest/1609/problem/E Tags: bitmasks, data structures, dp, matrices, *2400 Ref: https://codeforces.com/contest/1609/submission/137752276

---
+++ codeforces/1609/E.cpp
@@ -0,0 +1,210 @@
+#include <bits/stdc++.h>
+
+#include <algorithm>
+#include <cassert>
+#include <vector>
+
+
+#ifdef _MSC_VER
+#include <intrin.h>
+#endif
+
+namespace atcoder {
+
+namespace internal {
+
+int ceil_pow2(int n) {
+    int x = 0;
+    while ((1U << x) < (unsigned int)(n)) x++;
+    return x;
+}
+
+constexpr int bsf_constexpr(unsigned int n) {
+    int x = 0;
+    while (!(n & (1 << x))) x++;
+    return x;
+}
+
+int bsf(unsigned int n) {
+#ifdef _MSC_VER
+    unsigned long index;
+    _BitScanForward(&index, n);
+    return index;
+#else
+    return __builtin_ctz(n);
+#endif
+}
+
+}  // namespace internal
+
+}  // namespace atcoder
+
+
+namespace atcoder {
+
+template <class S, S (*op)(S, S), S (*e)()> struct segtree {
+  public:
+    segtree() : segtree(0) {}
+    explicit segtree(int n) : segtree(std::vector<S>(n, e())) {}
+    explicit segtree(const std::vector<S>& v) : _n(int(v.size())) {
+        log = internal::ceil_pow2(_n);
+        size = 1 << log;
+        d = std::vector<S>(2 * size, e());
+        for (int i = 0; i < _n; i++) d[size + i] = v[i];
+        for (int i = size - 1; i >= 1; i--) {
+            update(i);
+        }
+    }
+
+    void set(int p, S x) {
+        assert(0 <= p && p < _n);
+        p += size;
+        d[p] = x;
+        for (int i = 1; i <= log; i++) update(p >> i);
+    }
+
+    S get(int p) const {
+        assert(0 <= p && p < _n);
+        return d[p + size];
+    }
+
+    S prod(int l, int r) const {
+        assert(0 <= l && l <= r && r <= _n);
+        S sml = e(), smr = e();
+        l += size;
+        r += size;
+
+        while (l < r) {
+            if (l & 1) sml = op(sml, d[l++]);
+            if (r & 1) smr = op(d[--r], smr);
+            l >>= 1;
+            r >>= 1;
+        }
+        return op(sml, smr);
+    }
+
+    S all_prod() const { return d[1]; }
+
+    template <bool (*f)(S)> int max_right(int l) const {
+        return max_right(l, [](S x) { return f(x); });
+    }
+    template <class F> int max_right(int l, F f) const {
+        assert(0 <= l && l <= _n);
+        assert(f(e()));
+        if (l == _n) return _n;
+        l += size;
+        S sm = e();
+        do {
+            while (l % 2 == 0) l >>= 1;
+            if (!f(op(sm, d[l]))) {
+                while (l < size) {
+                    l = (2 * l);
+                    if (f(op(sm, d[l]))) {
+                        sm = op(sm, d[l]);
+                        l++;
+                    }
+                }
+                return l - size;
+            }
+            sm = op(sm, d[l]);
+            l++;
+        } while ((l & -l) != l);
+        return _n;
+    }
+
+    template <bool (*f)(S)> int min_left(int r) const {
+        return min_left(r, [](S x) { return f(x); });
+    }
+    template <class F> int min_left(int r, F f) const {
+        assert(0 <= r && r <= _n);
+        assert(f(e()));
+        if (r == 0) return 0;
+        r += size;
+        S sm = e();
+        do {
+            r--;
+            while (r > 1 && (r % 2)) r >>= 1;
+            if (!f(op(d[r], sm))) {
+                while (r < size) {
+                    r = (2 * r + 1);
+                    if (f(op(d[r], sm))) {
+                        sm = op(d[r], sm);
+                        r--;
+                    }
+                }
+                return r + 1 - size;
+            }
+            sm = op(d[r], sm);
+        } while ((r & -r) != r);
+        return 0;
+    }
+
+  private:
+    int _n, size, log;
+    std::vector<S> d;
+
+    void update(int k) { d[k] = op(d[2 * k], d[2 * k + 1]); }
+};
+
+}  // namespace atcoder
+
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+
+template<class T> bool uin(T& a, const T& b) { return a > b ? a = b, true : false; }
+template<class T> bool uax(T& a, const T& b) { return a < b ? a = b, true : false; }
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+constexpr int inf = 1e9;
+
+struct S {
+	int a, b, c, ab, bc, abc;
+	S() : a{0}, b{0}, c{0}, ab{0}, bc{0}, abc{0} { }
+	S(char x) : S() {
+		if (x == 'a') a = 1;
+		if (x == 'b') b = 1;
+		if (x == 'c') c = 1;
+	}
+};
+
+S op(S l, S r) {
+	S v;
+	v.a = l.a + r.a;
+	v.b = l.b + r.b;
+	v.c = l.c + r.c;
+	v.ab = max(l.ab + r.ab, min(l.a + r.ab, l.ab + r.b));
+	v.bc = max(l.bc + r.bc, min(l.b + r.bc, l.bc + r.c));
+	v.abc = min({l.a + r.abc, l.ab + r.bc, l.abc + r.c});
+	return v;
+}
+
+S e() { return S(); }
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n, q; cin >> n >> q;
+	string s; cin >> s;
+
+	atcoder::segtree<S, op, e> t(n);
+	for (int i = 0; i < n; ++i) {
+		t.set(i, S(s[i]));
+	}
+
+	while (q--) {
+		int i; char c; cin >> i >> c; --i;
+		t.set(i, S(c));
+		auto z = t.prod(0, n);
+		cout << z.abc << '\n';
+	}
+}

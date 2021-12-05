98656025efa4341758179a4656b070b5019817b5
fricai <rishabh.dhiman.145@gmail.com>
2021-12-05 17:49:00 +0530

    Add solution for problem `D - Sum of Maximum Weights` Link: https://atcoder.jp/contests/abc214/tasks/abc214_d Tags: AtCoder, *400 Ref: https://atcoder.jp/contests/abc214/submissions/27715817

---
+++ atcoder/abc214/D.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#include <atcoder/dsu>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+
+template<class T> bool uin(T& a, const T& b) { return a > b ? a = b, true : false; }
+template<class T> bool uax(T& a, const T& b) { return a < b ? a = b, true : false; }
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n; cin >> n;
+
+	vector<tuple<ll, int, int>> edges(n - 1);
+	for (auto &[w, u, v] : edges)
+		cin >> u >> v >> w, --u, --v;
+	sort(all(edges));
+
+	atcoder::dsu D(n);
+	vector<int> s(n, 1);
+
+	ll ans = 0;
+	for (auto [w, u, v] : edges) {
+		u = D.leader(u);
+		v = D.leader(v);
+		ans += w * s[u] * s[v];
+		D.merge(u, v);
+		s[D.leader(u)] = s[u] + s[v];
+	}
+
+	cout << ans << '\n';
+}

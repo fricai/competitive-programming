d39a81f4a5e4c31d10bcfb5abee56ebe04de8089
fricai <75023687+fricai@users.noreply.github.com>
2021-12-05 17:39:00 +0530

    Add solution for problem `B - How many?` Link: https://atcoder.jp/contests/abc214/tasks/abc214_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc214/submissions/27713892

---
+++ atcoder/abc214/B.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+
+template<class T> bool uin(T& a, const T& b) { return a > b ? a = b, true : false; }
+template<class T> bool uax(T& a, const T& b) { return a < b ? a = b, true : false; }
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int s, t; cin >> s >> t;
+
+	int ans = 0;
+	for (int a = 0; a <= s; ++a)
+		for (int b = 0; a + b <= s; ++b)
+			for (int c = 0; a + b + c <= s && a * b * c <= t; ++c)
+				++ans;
+	cout << ans << '\n';
+}

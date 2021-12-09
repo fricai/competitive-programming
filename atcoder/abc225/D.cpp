d8c4922c025534b0e81e639b3d73ce7e084b9b62
fricai <75023687+fricai@users.noreply.github.com>
2021-12-09 17:41:00 +0530

    Add solution for problem `D - Play Train` Link: https://atcoder.jp/contests/abc225/tasks/abc225_d Tags: AtCoder, *400 Ref: https://atcoder.jp/contests/abc225/submissions/27781484

---
+++ atcoder/abc225/D.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+
+template<class T> bool uin(T& a, const T& b) { return a > b ? a = b, true : false; }
+template<class T> bool uax(T& a, const T& b) { return a < b ? a = b, true : false; }
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n, q; cin >> n >> q;
+
+	vector<int> pre(n, -1), nxt(n, -1);
+	while (q--) {
+		int t; cin >> t;
+		if (t != 3) {
+			int x, y; cin >> x >> y; --x; --y;
+			if (t == 1) {
+				nxt[y] = x;
+				pre[x] = y;
+			} else {
+				nxt[y] = -1;
+				pre[x] = -1;
+			}
+		} else {
+			int x; cin >> x; --x;
+			while (nxt[x] != -1) x = nxt[x];
+
+			vector<int> ans;
+			while (x != -1) {
+				ans.push_back(x);
+				x = pre[x];
+			}
+
+			cout << sz(ans) << ' ';
+			for (auto x : ans)
+				cout << x + 1 << ' ';
+			cout << '\n';
+		}
+	}
+}

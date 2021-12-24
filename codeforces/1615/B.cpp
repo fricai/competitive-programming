4389fccc85d7c399d7156c3d22ea4e27da10a404
fricai <rishabh.dhiman.145@gmail.com>
2021-12-24 20:22:00 +0530

    Add solution for problem `B - And It's Non-Zero` Link: https://codeforces.com/contest/1615/problem/B Tags: bitmasks, greedy Ref: https://codeforces.com/contest/1615/submission/140461257

---
+++ codeforces/1615/B.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+
+template<class T> bool uin(T& a, const T& b) { return a > b ? a = b, true : false; }
+template<class T> bool uax(T& a, const T& b) { return a < b ? a = b, true : false; }
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+namespace internal {
+unsigned long long floor_sum_unsigned(unsigned long long n,
+                                      unsigned long long m,
+                                      unsigned long long a,
+                                      unsigned long long b) {
+    unsigned long long ans = 0;
+    while (true) {
+        if (a >= m) {
+            ans += n * (n - 1) / 2 * (a / m);
+            a %= m;
+        }
+        if (b >= m) {
+            ans += n * (b / m);
+            b %= m;
+        }
+
+        unsigned long long y_max = a * n + b;
+        if (y_max < m) break;
+        // y_max < m * (n + 1)
+        // floor(y_max / m) <= n
+        n = (unsigned long long)(y_max / m);
+        b = (unsigned long long)(y_max % m);
+        std::swap(m, a);
+    }
+    return ans;
+}
+
+constexpr long long safe_mod(long long x, long long m) {
+    x %= m;
+    if (x < 0) x += m;
+    return x;
+}
+}
+
+long long floor_sum(long long n, long long m, long long a, long long b) {
+    assert(0 <= n && n < (1LL << 32));
+    assert(1 <= m && m < (1LL << 32));
+    unsigned long long ans = 0;
+    if (a < 0) {
+        unsigned long long a2 = internal::safe_mod(a, m);
+        ans -= 1ULL * n * (n - 1) / 2 * ((a2 - a) / m);
+        a = a2;
+    }
+    if (b < 0) {
+        unsigned long long b2 = internal::safe_mod(b, m);
+        ans -= 1ULL * n * ((b2 - b) / m);
+        b = b2;
+    }
+    return ans + internal::floor_sum_unsigned(n, m, a, b);
+}
+
+int solve() {
+	int l, r; cin >> l >> r; --l;
+
+	ll ans = r - l;
+	rep(b, 0, 25) {
+		if (r < (1 << b))
+			break;
+		uin(ans, (r - l) - (
+				floor_sum(1 << b, 2 << b, -1, r - (1 << b))
+				-
+				floor_sum(1 << b, 2 << b, -1, l - (1 << b))
+				)
+		   )
+				;
+	}
+	return ans;
+}
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int t; cin >> t;
+	while (t--) cout << solve() << '\n';
+}

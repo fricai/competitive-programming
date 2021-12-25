83fc72f8757dae14e4b83010331057e2354f2bff
fricai <75023687+fricai@users.noreply.github.com>
2021-12-25 17:33:00 +0530

    Add solution for problem `B - A Reverse` Link: https://atcoder.jp/contests/abc233/tasks/abc233_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc233/submissions/28113980

---
+++ atcoder/abc233/B.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) int((x).size())
+
+template<class T> bool uin(T& a, const T& b) { return a > b ? a = b, true : false; }
+template<class T> bool uax(T& a, const T& b) { return a < b ? a = b, true : false; }
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int l, r; cin >> l >> r; --l; --r;
+	string s; cin >> s;
+	for (int i = l, j = r; i <= j; ++i, --j) {
+		swap(s[i], s[j]);
+	}
+	cout << s << '\n';
+}

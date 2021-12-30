f72d55775d5787b8bc636f037cf56f80342f0db3
fricai <rishabh.dhiman.145@gmail.com>
2021-12-30 18:24:00 +0530

    Add solution for problem `B - Balls of Three Colors` Link: https://atcoder.jp/contests/arc128/tasks/arc128_b Tags: AtCoder, *400 Ref: https://atcoder.jp/contests/arc128/submissions/28239328

---
+++ atcoder/arc128/B.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) static_cast<int>((x).size())
+
+template<class T> bool uin(T& a, const T& b) { return a > b ? a = b, true : false; }
+template<class T> bool uax(T& a, const T& b) { return a < b ? a = b, true : false; }
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+int solve() {
+	array<int, 3> a;
+	for (auto &y : a) cin >> y;
+	sort(all(a));
+	do {
+		int x = a[0], y = a[1], z = a[2];
+		if ((x - y) % 3 != 0 || x > y)
+			continue;
+		return y;
+	} while (next_permutation(all(a)));
+	return -1;
+}
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int t; cin >> t;
+	while (t--) cout << solve() << '\n';
+}

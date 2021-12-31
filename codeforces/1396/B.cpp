29127dbfbf32e9664f29f0cc3e17e8038f83ad08
fricai <rishabh.dhiman.145@gmail.com>
2021-12-31 19:16:00 +0530

    Add solution for problem `B - Stoned Game` Link: https://codeforces.com/contest/1396/problem/B Tags: brute force, constructive algorithms, games, greedy, *1800 Ref: https://codeforces.com/contest/1396/submission/141247951

---
+++ codeforces/1396/B.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) static_cast<int>((x).size())
+
+template<class T> bool uin(T& a, const T& b) { return a > b ? a = b, true : false; }
+template<class T> bool uax(T& a, const T& b) { return a < b ? a = b, true : false; }
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+bool solve() {
+	int n; cin >> n;
+	vector<int> a(n);
+	for (auto &x : a)
+		cin >> x;
+
+	int s = accumulate(all(a), 0);
+	if (*max_element(all(a)) > s / 2)
+		return 1;
+	return s & 1;
+}
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int t; cin >> t;
+	while (t--) cout << (solve() ? "T" : "HL") << '\n';
+}

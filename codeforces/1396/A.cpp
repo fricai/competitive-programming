02bcbfa9cdef7867917aaefad0eae5c00d0f228e
fricai <75023687+fricai@users.noreply.github.com>
2021-12-31 19:03:00 +0530

    Add solution for problem `A - Multiples of Length` Link: https://codeforces.com/contest/1396/problem/A Tags: constructive algorithms, greedy, number theory, *1600 Ref: https://codeforces.com/contest/1396/submission/141246857

---
+++ codeforces/1396/A.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) static_cast<int>((x).size())
+
+template<class T> bool uin(T& a, const T& b) { return a > b ? a = b, true : false; }
+template<class T> bool uax(T& a, const T& b) { return a < b ? a = b, true : false; }
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n; cin >> n;
+	vector<ll> a(n);
+	for (auto &x : a)
+		cin >> x;
+
+	if (n == 1) {
+		cout << "1 1\n0\n1 1\n0\n1 1\n" << -a[0] << '\n';
+		return 0;
+	}
+
+	cout << "1 " << n << '\n';
+	cout << "0";
+	rep(i, 1, n) cout << ' ' << -n * a[i];
+	cout << '\n';
+
+	cout << "2 " << n << '\n';
+	rep(i, 1, n) cout << (n - 1) * a[i] << ' ';
+	cout << '\n';
+
+	cout << "1 1\n" << -a[0] << '\n';
+}

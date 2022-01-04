d70f0c8cc5f339b03cc2c3433325e01f750f63ff
fricai <rishabh.dhiman.145@gmail.com>
2022-01-04 21:08:00 +0530

    Add solution for problem `A - A.M. Deviation` Link: https://codeforces.com/contest/1605/problem/A Tags: math, number theory, *800 Ref: https://codeforces.com/contest/1605/submission/141657825

---
+++ codeforces/1605/A.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) static_cast<int>((x).size())
+
+template<class T> bool uin(T& a, const T& b) { return a > b ? a = b, true : false; }
+template<class T> bool uax(T& a, const T& b) { return a < b ? a = b, true : false; }
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+int solve() {
+	 int a, b, c; cin >> a >> b >> c;
+	 return (a + b + c) % 3 != 0;
+}
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int t; cin >> t;
+	while (t--) cout << solve() << '\n';
+}

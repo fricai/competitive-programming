2dc54bfb3b786cf92786593592372728c0cab7a4
fricai <75023687+fricai@users.noreply.github.com>
2022-01-11 03:29:00 +0530

    Add solution for problem `B - Make AP` Link: https://codeforces.com/contest/1624/problem/B Tags: implementation, math, *900 Ref: https://codeforces.com/contest/1624/submission/142310971

---
+++ codeforces/1624/B.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) static_cast<int>((x).size())
+
+template<class T> bool uin(T& a, const T& b) { return a > b ? a = b, true : false; }
+template<class T> bool uax(T& a, const T& b) { return a < b ? a = b, true : false; }
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+bool solve() {
+	int a, b, c; cin >> a >> b >> c;
+	return
+		(a + c) % (2 * b) == 0 || 
+		(2 * b - a > 0 && (2 * b - a) % c == 0) ||
+		(2 * b - c > 0 && (2 * b - c) % a == 0)
+		;
+}
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int t; cin >> t;
+	while (t--) cout << (solve() ? "YES" : "NO") << '\n';
+}

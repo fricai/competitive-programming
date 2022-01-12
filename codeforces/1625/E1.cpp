d993e0932bde314a4cdf02ce566424b09097df96
fricai <75023687+fricai@users.noreply.github.com>
2022-01-13 00:59:00 +0530

    Add solution for problem `E1 - Cats on the Upgrade (easy version)` Link: https://codeforces.com/contest/1625/problem/E1 Tags: brute force, data structures, dfs and similar, divide and conquer, dp, graphs, trees, *2500 Ref: https://codeforces.com/contest/1625/submission/142545810

--- codeforces/1625/E1.cpp
+++ codeforces/1625/E1.cpp
@@ -1,7 +1,7 @@
+#include <bits/stdc++.h>
+
 #pragma GCC optimize("O3,unroll-loops")
 #pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
-
-#include <bits/stdc++.h>
 
 using namespace std;
 using ll = long long;

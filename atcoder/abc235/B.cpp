a6d90f5869c41dd00e368cddc2af31abc961743a
fricai <rishabh.dhiman.145@gmail.com>
2022-01-15 17:33:00 +0530

    Add solution for problem `B - Climbing Takahashi` Link: https://atcoder.jp/contests/abc235/tasks/abc235_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc235/submissions/28529284

---
+++ atcoder/abc235/B.cpp
@@ -0,0 +1,32 @@
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) static_cast<int>((x).size())
+
+template<class T> bool uin(T& a, const T& b) { return a > b ? a = b, true : false; }
+template<class T> bool uax(T& a, const T& b) { return a < b ? a = b, true : false; }
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n; cin >> n;
+	vector<int> h(n);
+	for (auto &x : h) cin >> x;
+
+	int i = 0;
+	while (i < n && h[i + 1] > h[i]) ++i;
+	cout << h[min(n - 1, i)] << '\n';
+}

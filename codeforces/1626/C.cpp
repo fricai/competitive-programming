6e653404ec25cccf2af3323b3a5631aa13b53522
fricai <75023687+fricai@users.noreply.github.com>
2022-01-17 00:03:00 +0530

    Add solution for problem `C - Monsters And Spells` Link: https://codeforces.com/contest/1626/problem/C Tags: binary search, data structures, dp, greedy, implementation, math, two pointers, *1700 Ref: https://codeforces.com/contest/1626/submission/143035631

---
+++ codeforces/1626/C.cpp
@@ -0,0 +1,60 @@
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) static_cast<int>((x).size())
+
+template<class T> bool uin(T& a, const T& b) { return a > b ? a = b, true : false; }
+template<class T> bool uax(T& a, const T& b) { return a < b ? a = b, true : false; }
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+using pii = pair<int, int>;
+set<pii>::iterator addInterval(set<pii>& is, int L, int R) {
+	if (L == R) return is.end();
+	auto it = is.lower_bound({L, R}), before = it;
+	while (it != is.end() && it->first <= R) {
+		R = max(R, it->second);
+		before = it = is.erase(it);
+	}
+	if (it != is.begin() && (--it)->second > L) {
+		L = min(L, it->first);
+		R = max(R, it->second);
+		is.erase(it);
+	}
+	return is.insert(before, {L,R});
+}
+
+ll solve() {
+	int n; cin >> n;
+	vector<int> k(n), h(n);
+	for (auto &x : k) cin >> x;
+	for (auto &x : h) cin >> x;
+
+	set<pii> s;
+	rep(i, 0, n) addInterval(s, k[i] - h[i], k[i]);
+
+	ll ans = 0;
+	for (auto [x, y] : s) {
+		ll del = y - x;
+		ans += del * (del + 1) / 2;
+	}
+	return ans;
+}
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int t; cin >> t;
+	while (t--) cout << solve() << '\n';
+}

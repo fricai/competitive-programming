5f96d0b785deb2384b565f7c2b7ad6d2d218644f
fricai <75023687+fricai@users.noreply.github.com>
2022-01-23 20:51:00 +0530

    Add solution for problem `A - Erase by Value` Link: https://atcoder.jp/contests/arc133/tasks/arc133_a Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/arc133/submissions/28762278

---
+++ atcoder/arc133/A.cpp
@@ -0,0 +1,36 @@
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) static_cast<int>((x).size())
+
+template<class T> bool uin(T& a, const T& b) { return a > b ? a = b, true : false; }
+template<class T> bool uax(T& a, const T& b) { return a < b ? a = b, true : false; }
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n; cin >> n;
+	vector<int> a(n);
+	for (auto &x : a) cin >> x;
+
+	int j = 0;
+	while (j < n - 1 && a[j] <= a[j + 1]) ++j;
+
+	for (auto x : a)
+		if (x != a[j])
+			cout << x << ' ';
+	cout << '\n';
+}

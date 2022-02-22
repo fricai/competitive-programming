10bd29ec46692eee0820b3e33cfba07f1dae4aaf
fricai <rishabh.dhiman.145@gmail.com>
2022-02-22 23:47:00 +0530

    Add solution for problem `D - Cross Coloring` Link: https://codeforces.com/contest/1644/problem/D Tags: data structures, implementation, math, *1700 Ref: https://codeforces.com/contest/1644/submission/147360580

--- codeforces/1644/D.cpp
+++ codeforces/1644/D.cpp
@@ -1,5 +1,5 @@
-#pragma GCC optimize("O3,unroll-loops")
-#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+//#pragma GCC optimize("O3,unroll-loops")
+//#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
 
 #include "bits/stdc++.h"
 

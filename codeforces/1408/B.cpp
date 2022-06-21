7af6c7e593547f32843ec160c593dfa0ad4f72c4
fricai <75023687+fricai@users.noreply.github.com>
2022-06-21 21:49:00 +0530

    Add solution for problem `B - Arrays Sum` Link: https://codeforces.com/contest/1408/problem/B Tags: constructive algorithms, greedy, math, *1400 Ref: https://codeforces.com/contest/1408/submission/161410302

--- codeforces/1408/B.cpp
+++ codeforces/1408/B.cpp
@@ -1,38 +1,51 @@
-#include <bits/stdc++.h>
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+#include "bits/stdc++.h"
 
 using namespace std;
 using ll = long long;
 using ld = long double;
 
-#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
-#define per(i, a, b) for (auto i = (b); i-- > (a); )
-#define eb(x...) emplace_back(x)
+#define rep(i, a, b) for (auto i{a}; i < (b); ++i)
+#define per(i, a, b) for (auto i{b}; i-- > (a);)
 #define all(x) begin(x), end(x)
 #define rall(x) (x).rbegin(), (x).rend()
-#define sz(x) int((x).size())
-#define lb(x...) lower_bound(x)
-#define ub(x...) upper_bound(x)
+#define sz(x) static_cast<int>((x).size())
 
-template<class T> bool ckmin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
-template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
-template<class T> T gcd(T a, T b) { return b != T(0) ? gcd(b, a % b) : a; }
+template <class T>
+bool uin(T& a, const T& b) {
+    return a > b ? a = b, true : false;
+}
+template <class T>
+bool uax(T& a, const T& b) {
+    return a < b ? a = b, true : false;
+}
 
-const int N = 1 << 7;
-int a[N], b[N][N];
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 
 int solve() {
-	int n, k; cin >> n >> k;
-	rep(i, 0, n) cin >> a[i];
-	n = unique(a, a + n) - a;
-	if (n <= k) return 1;
-	if (k == 1) return -1;
-	return (n - 1 + k - 1 - 1) / (k - 1);
+    int n, k;
+    cin >> n >> k;
+    vector<int> a(n);
+    for (auto& x : a) cin >> x;
+
+    n = unique(all(a)) - begin(a);
+
+    int ans = 0;
+    n -= k;
+    ++ans;
+    if (n <= 0) return 1;
+    if (k == 1) return -1;
+    while (n > 0) ++ans, n -= k - 1;
+    return ans;
 }
 
 signed main() {
-	ios::sync_with_stdio(false);
-	cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-	int t; cin >> t;
-	while (t--) cout << solve() << '\n';
+    int t;
+    cin >> t;
+    while (t--) cout << solve() << '\n';
 }

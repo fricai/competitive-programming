e9cac1cfba2b85490ea6279c503c92ebc389a04c
fricai <75023687+fricai@users.noreply.github.com>
2022-06-26 00:48:00 +0530

    Add solution for problem `G - Fishingprince Plays With Array Again` Link: https://codeforces.com/contest/1696/problem/G Tags: brute force, data structures, geometry, math, *3300 Ref: https://codeforces.com/contest/1696/submission/161808790

---
+++ codeforces/1696/G.cpp
@@ -0,0 +1,242 @@
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+#include "bits/stdc++.h"
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i{a}; i < (b); ++i)
+#define per(i, a, b) for (auto i{b}; i-- > (a);)
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) static_cast<int>((x).size())
+
+template <class T>
+bool uin(T& a, const T& b) {
+    return a > b ? a = b, true : false;
+}
+template <class T>
+bool uax(T& a, const T& b) {
+    return a < b ? a = b, true : false;
+}
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+
+#include <algorithm>
+#include <cassert>
+#include <vector>
+
+
+#ifdef _MSC_VER
+#include <intrin.h>
+#endif
+
+namespace atcoder {
+
+namespace internal {
+
+int ceil_pow2(int n) {
+    int x = 0;
+    while ((1U << x) < (unsigned int)(n)) x++;
+    return x;
+}
+
+constexpr int bsf_constexpr(unsigned int n) {
+    int x = 0;
+    while (!(n & (1 << x))) x++;
+    return x;
+}
+
+int bsf(unsigned int n) {
+#ifdef _MSC_VER
+    unsigned long index;
+    _BitScanForward(&index, n);
+    return index;
+#else
+    return __builtin_ctz(n);
+#endif
+}
+
+}  // namespace internal
+
+}  // namespace atcoder
+
+
+namespace atcoder {
+
+template <class S, S (*op)(S, S), S (*e)()> struct segtree {
+  public:
+    segtree() : segtree(0) {}
+    explicit segtree(int n) : segtree(std::vector<S>(n, e())) {}
+    explicit segtree(const std::vector<S>& v) : _n(int(v.size())) {
+        log = internal::ceil_pow2(_n);
+        size = 1 << log;
+        d = std::vector<S>(2 * size, e());
+        for (int i = 0; i < _n; i++) d[size + i] = v[i];
+        for (int i = size - 1; i >= 1; i--) {
+            update(i);
+        }
+    }
+
+    void set(int p, S x) {
+        assert(0 <= p && p < _n);
+        p += size;
+        d[p] = x;
+        for (int i = 1; i <= log; i++) update(p >> i);
+    }
+
+    S get(int p) const {
+        assert(0 <= p && p < _n);
+        return d[p + size];
+    }
+
+    S prod(int l, int r) const {
+        assert(0 <= l && l <= r && r <= _n);
+        S sml = e(), smr = e();
+        l += size;
+        r += size;
+
+        while (l < r) {
+            if (l & 1) sml = op(sml, d[l++]);
+            if (r & 1) smr = op(d[--r], smr);
+            l >>= 1;
+            r >>= 1;
+        }
+        return op(sml, smr);
+    }
+
+    S all_prod() const { return d[1]; }
+
+    template <bool (*f)(S)> int max_right(int l) const {
+        return max_right(l, [](S x) { return f(x); });
+    }
+    template <class F> int max_right(int l, F f) const {
+        assert(0 <= l && l <= _n);
+        assert(f(e()));
+        if (l == _n) return _n;
+        l += size;
+        S sm = e();
+        do {
+            while (l % 2 == 0) l >>= 1;
+            if (!f(op(sm, d[l]))) {
+                while (l < size) {
+                    l = (2 * l);
+                    if (f(op(sm, d[l]))) {
+                        sm = op(sm, d[l]);
+                        l++;
+                    }
+                }
+                return l - size;
+            }
+            sm = op(sm, d[l]);
+            l++;
+        } while ((l & -l) != l);
+        return _n;
+    }
+
+    template <bool (*f)(S)> int min_left(int r) const {
+        return min_left(r, [](S x) { return f(x); });
+    }
+    template <class F> int min_left(int r, F f) const {
+        assert(0 <= r && r <= _n);
+        assert(f(e()));
+        if (r == 0) return 0;
+        r += size;
+        S sm = e();
+        do {
+            r--;
+            while (r > 1 && (r % 2)) r >>= 1;
+            if (!f(op(d[r], sm))) {
+                while (r < size) {
+                    r = (2 * r + 1);
+                    if (f(op(d[r], sm))) {
+                        sm = op(d[r], sm);
+                        r--;
+                    }
+                }
+                return r + 1 - size;
+            }
+            sm = op(d[r], sm);
+        } while ((r & -r) != r);
+        return 0;
+    }
+
+  private:
+    int _n, size, log;
+    std::vector<S> d;
+
+    void update(int k) { d[k] = op(d[2 * k], d[2 * k + 1]); }
+};
+
+}  // namespace atcoder
+
+
+int s, t;
+
+constexpr double inf = 1e18;
+struct mat {
+    array<array<double, 3>, 3> a;
+    mat() {
+        for (auto& v : a) fill(all(v), -inf);
+        rep(i, 0, 3) a[i][i] = 0;
+    }
+    mat(double x) {
+        a[0][0] = a[0][1] = a[0][2] = 0;
+        a[1][0] = a[1][1] = x / (s + t);
+        a[2][0] = x / t;
+
+        a[1][2] = -inf;
+        a[2][1] = a[2][2] = -inf;
+    }
+};
+mat op(mat a, mat b) {
+    mat c;
+    rep(i, 0, 3) rep(j, 0, 3) rep(k, 0, 3) uax(c.a[i][j], a.a[i][k] + b.a[k][j]);
+    return c;
+}
+mat e() { return mat(); }
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, q;
+    cin >> n >> q;
+
+    cin >> s >> t;
+    if (s > t) swap(s, t);
+    // WLOG s <= t
+
+    vector<int> a(n);
+    for (auto& x : a) cin >> x;
+
+    atcoder::segtree<mat, op, e> t(n);
+    rep(i, 0, n) t.set(i, a[i]);
+
+    cout << fixed << setprecision(10);
+    while (q--) {
+        int type;
+        cin >> type;
+
+        if (type == 1) {
+            int k;
+            cin >> k;
+            --k;
+            cin >> a[k];
+            t.set(k, a[k]);
+        } else {
+            int l, r;
+            cin >> l >> r;
+            --l;
+
+            const auto x = t.prod(l, r);
+            double ans = 0;
+
+            rep(i, 0, 3) rep(j, 0, 3) uax(ans, x.a[i][j]);
+            cout << ans << '\n';
+        }
+    }
+}

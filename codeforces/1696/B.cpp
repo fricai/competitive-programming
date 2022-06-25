d54d8cfe73a25d7766ef99bb3e737f44e34b583c
fricai <75023687+fricai@users.noreply.github.com>
2022-06-25 20:09:00 +0530

    Add solution for problem `B - NIT Destroys the Universe` Link: https://codeforces.com/contest/1696/problem/B Tags: greedy, *900 Ref: https://codeforces.com/contest/1696/submission/161735346

---
+++ codeforces/1696/B.cpp
@@ -0,0 +1,51 @@
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+#include "bits/stdc++.h"
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i{a}; i < (b); ++i)
+#define per(i, a, b) for (auto i{b}; i-- > (a);)
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) static_cast<int>((x).size())
+
+template <class T>
+bool uin(T& a, const T& b) {
+    return a > b ? a = b, true : false;
+}
+template <class T>
+bool uax(T& a, const T& b) {
+    return a < b ? a = b, true : false;
+}
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+int solve() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (auto& x : a) cin >> x;
+
+    int ans = 0;
+    for (int l = 0, r = 0; l < n; l = r) {
+        if (a[l] == 0) {
+            ++r;
+            continue;
+        }
+        while (r < n && a[r] != 0) ++r;
+        ++ans;
+    }
+    return min(2, ans);
+}
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--) cout << solve() << '\n';
+}

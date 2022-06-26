658adf44ba8dc3b57254662d25636d73292e5198
fricai <75023687+fricai@users.noreply.github.com>
2022-06-26 20:40:00 +0530

    Add solution for problem `A - Reverse and Minimize` Link: https://atcoder.jp/contests/arc142/tasks/arc142_a Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/arc142/submissions/32784228

---
+++ atcoder/arc142/A.cpp
@@ -0,0 +1,45 @@
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+#include "bits/stdc++.h"
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i{a}; i < (b); ++i)
+#define per(i, a, b) for (auto i{b}; i-- > (a);)
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) static_cast<int>((x).size())
+
+template <class T>
+bool uin(T& a, const T& b) {
+    return a > b ? a = b, true : false;
+}
+template <class T>
+bool uax(T& a, const T& b) {
+    return a < b ? a = b, true : false;
+}
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    ll n, k;
+    cin >> n >> k;
+
+    const auto str_k = to_string(k);
+    const auto rev_k = stoll(string(rall(str_k)));
+    if (k > rev_k) return cout << "0\n", 0;
+
+    ll cnt = 0;
+    for (ll p = 1; k * p <= n; p *= 10) ++cnt;
+    if (k != rev_k) {
+        k = rev_k;
+        for (ll p = 1; k * p <= n; p *= 10) ++cnt;
+    }
+    cout << cnt << '\n';
+}

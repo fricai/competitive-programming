5b4989334692c0c4517097d869fae5db60754e51
fricai <75023687+fricai@users.noreply.github.com>
2022-06-27 02:16:00 +0530

    Add solution for problem `G - Sqrt` Link: https://atcoder.jp/contests/abc243/tasks/abc243_g Tags: AtCoder, *600 Ref: https://atcoder.jp/contests/abc243/submissions/32786961

---
+++ atcoder/abc243/G.cpp
@@ -0,0 +1,58 @@
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+#include "bits/stdc++.h"
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+#define rep(i, a, b) for (auto i{a}; i < (b); ++i)
+#define per(i, a, b) for (auto i{b}; i-- > (a);)
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) static_cast<int>((x).size())
+
+template <class T>
+bool uin(T& a, const T& b) {
+    return a > b ? a = b, true : false;
+}
+template <class T>
+bool uax(T& a, const T& b) {
+    return a < b ? a = b, true : false;
+}
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+constexpr int N = 55'000;
+ll f[N];
+
+ll sq(ll x) {
+    ll z = sqrt(x);
+    while (z * z < x) ++z;
+    while (z * z > x) --z;
+    assert(z * z <= x && x < (z + 1) * (z + 1));
+    return z;
+}
+
+ll solve() {
+    ll x;
+    cin >> x;
+    const ll s = sq(x);
+    ll ans = 0;
+    for (ll z = 1; z * z <= s; ++z) ans += f[z] * (s - z * z + 1);
+    return ans;
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    f[1] = 1;
+    for (int i = 2; i < N; ++i)
+        for (int j = 1; j * j <= i; ++j) f[i] += f[j];
+
+    int t;
+    cin >> t;
+    while (t--) cout << solve() << '\n';
+}

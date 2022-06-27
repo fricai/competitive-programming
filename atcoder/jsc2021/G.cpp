5c399855fb4c266707789c820325888fa9d0a0fb
fricai <75023687+fricai@users.noreply.github.com>
2022-06-27 20:07:00 +0530

    Add solution for problem `G - Spanning Tree` Link: https://atcoder.jp/contests/jsc2021/tasks/jsc2021_g Tags: AtCoder, *600 Ref: https://atcoder.jp/contests/jsc2021/submissions/32802294

--- atcoder/jsc2021/G.cpp
+++ atcoder/jsc2021/G.cpp
@@ -1,87 +1,70 @@
-#include <atcoder/modint>
-#include <bits/stdc++.h>
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+#include "bits/stdc++.h"
 
 using namespace std;
 using ll = long long;
 using ld = long double;
 
-#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
-#define per(i, a, b) for (auto i = (b); i-- > (a); )
+#define rep(i, a, b) for (auto i{a}; i < (b); ++i)
+#define per(i, a, b) for (auto i{b}; i-- > (a);)
 #define all(x) begin(x), end(x)
 #define rall(x) (x).rbegin(), (x).rend()
-#define sz(x) int((x).size())
-#define lb(x...) lower_bound(x)
-#define ub(x...) upper_bound(x)
+#define sz(x) static_cast<int>((x).size())
 
-template<class T> bool ckmin(T& a, const T& b) { return a > b ? a = b, 1 : 0; }
-template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+template <class T>
+bool uin(T& a, const T& b) {
+    return a > b ? a = b, true : false;
+}
+template <class T>
+bool uax(T& a, const T& b) {
+    return a < b ? a = b, true : false;
+}
 
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 
-const int N = 300 + 2, M = 1e9 + 7;
-using mint = atcoder::static_modint<M>;
-
-mint det(vector<vector<mint>>& a) {
-	int n = sz(a);
-
-	mint res = 1;
-	rep(i, 0, n) {
-		int b = i;
-		while (b < n && a[b][i].val() == 0) ++b;
-		
-		if (b == n) return 0;
-		if (i != b) swap(a[i], a[b]), res *= -1;
-
-		res *= a[i][i];
-		auto x = 1 / a[i][i];
-		rep(j, i + 1, n) {
-			mint v = a[j][i] * x;
-			rep(k, i + 1, n) a[j][k] -= v * a[i][k];
-		}
-	}
-	return res;
-}
-
-int a[N][N];
-int cc[N];
-int cnt = 0;
-int n;
-
-bool cycle(int u, int p) {
-	cc[u] = cnt;
-	rep(v, 0, n) {
-		if (v == p || a[u][v] != 1) continue;
-		if (cc[v] || cycle(v, u)) return 1;
-	}
-	return 0;
-}
+#include <atcoder/dsu>
+#include <atcoder/modint>
+using mint = atcoder::modint1000000007;
 
 signed main() {
-	ios::sync_with_stdio(false);
-	cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-	cin >> n;
+    int n;
+    cin >> n;
 
-	rep(i, 0, n) rep(j, 0, n) cin >> a[i][j];
-	rep(u, 0, n)
-		if (!cc[u]) {
-			++cnt;
-			if (cycle(u, -1)) return cout << "0\n", 0;
-		}
-	rep(u, 0, n) --cc[u];
+    vector a(n, vector<int>(n));
+    rep(i, 0, n) rep(j, 0, n) cin >> a[i][j];
 
-	vector<vector<mint>> L(cnt, vector<mint>(cnt, 0));
+    atcoder::dsu D(n);
+    rep(i, 0, n) rep(j, 0, i) {
+        if (a[i][j] != 1) continue;
+        if (D.same(i, j)) return cout << 0 << '\n', 0;
+        D.merge(i, j);
+    }
 
-	rep(u, 0, n) {
-		rep(v, 0, n) {
-			if (a[u][v] != -1) continue;
-			--L[cc[u]][cc[v]];
-			++L[cc[u]][cc[u]];
-		}
-	}
+    vector<int> v;
+    rep(i, 0, n) if (D.leader(i) == i) v.push_back(i);
+    const int m = sz(v);
+    auto hsh = [&](int i) { return lower_bound(all(v), D.leader(i)) - begin(v); };
 
-	L.pop_back();
-	for (auto &b : L) b.pop_back();
+    vector mat(m, valarray<mint>(m));
+    rep(i, 0, n) rep(j, 0, n) if (a[i][j] == -1)-- mat[hsh(i)][hsh(j)];
+    rep(i, 0, m) mat[i][i] -= accumulate(all(mat[i]), mint(0));
 
-	cout << det(L).val() << '\n';
+    mint res = 1;
+    rep(i, 0, m - 1) {
+        res *= mat[i][i];
+        if (mat[i][i] == 0) break;
+        const auto inv_val = mat[i][i].inv();
+        mat[i] *= inv_val;
+        rep(j, i + 1, m - 1) {
+            const auto to_sub = mat[j][i] * mat[i];
+            mat[j] -= to_sub;
+        }
+    }
+
+    cout << res.val() << '\n';
 }

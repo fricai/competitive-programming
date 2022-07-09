8bdd9c757d0a6231763f327cb63a6596f1044175
fricai <rishabh.dhiman.145@gmail.com>
2022-07-10 02:13:00 +0530

    Add solution for problem `E - New Year and Castle Construction` Link: https://codeforces.com/contest/1284/problem/E Tags: combinatorics, geometry, math, sortings, *2500 Ref: https://codeforces.com/contest/1284/submission/163409217

--- codeforces/1284/E.cpp
+++ codeforces/1284/E.cpp
@@ -66,19 +66,15 @@ signed main() {
         });
 
         ll cur = 0;
-
-        {
-            int j = 0, del = 0;
+        for (int i = 0, j = 0, del = 0; i < n - 1; ++i) {
+            --del;
             auto inc = [&]() {
                 ++j, ++del;
                 if (j == n - 1) j = 0;
             };
-            for (int i = 0; i < n - 1; ++i) {
-                --del;
-                if (j == i) inc();
-                while (cross(q[i], q[j]) > 0) inc();
-                cur += C(del, 3);
-            }
+            if (j == i) inc();
+            while (cross(q[i], q[j]) > 0) inc();
+            cur += C(del, 3);
         }
 
         ans += quads - cur;

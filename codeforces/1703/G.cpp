2b4097ad567a84c8e01ec932dd6dc0aa93356a94
fricai <rishabh.dhiman.145@gmail.com>
2022-07-12 21:12:00 +0530

    Add solution for problem `G - Good Key, Bad Key` Link: https://codeforces.com/contest/1703/problem/G Tags: bitmasks, brute force, dp, greedy, math Ref: https://codeforces.com/contest/1703/submission/163892907

--- codeforces/1703/G.cpp
+++ codeforces/1703/G.cpp
@@ -24,7 +24,8 @@ bool uax(T& a, const T& b) {
 
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 
-constexpr int MAX = 1e9 + 10;
+constexpr int B = 40;
+constexpr ll inf = 1e18;
 
 signed main() {
     ios::sync_with_stdio(false);
@@ -33,23 +34,25 @@ signed main() {
     int t;
     cin >> t;
     while (t--) {
-        int n, k;
+        int n;
+        ll k;
         cin >> n >> k;
 
         vector<ll> a(n);
         for (auto& x : a) cin >> x;
 
-        ll ans = 0;
-        ll sum = 0;
-        for (int i = 0; i < n; ++i) {
-            // [0, i) are opened with good keys
-            ll cur = sum;
-            for (int j = i, r = 1; (1ll << r) <= MAX && j < n; ++j, ++r) cur += a[j] >> r;
-            uax(ans, cur);
-
-            sum += a[i] - k;
+        vector dp(n + 1, array<ll, B>{});
+        dp[0][0] = 0;
+        // rep(j, 1, B) dp[0][j] = -inf;
+        // max cost if you have used the key j times already,
+        // 1. it was not part of the last use
+        rep(i, 0, n) {
+            dp[i + 1][0] = dp[i][0] + a[i] - k;
+            rep(j, 1, B) dp[i + 1][j] = max(dp[i][j] + (a[i] >> j) - k, dp[i][j - 1] + (a[i] >> j));
         }
-        uax(ans, sum);
+
+        ll ans = 0;
+        rep(i, 0, n + 1) rep(j, 0, B) uax(ans, dp[i][j]);
         cout << ans << '\n';
     }
 }
